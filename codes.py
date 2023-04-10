7be4d300e852fd0ee872868170b3c9017e992615
Replit user <>
2023-04-10 14:53:48 +0000

    added min_max.py

--- codes.py
+++ codes.py
@@ -1,15 +1,27 @@
-numbers = [1, 2, 3, 4, 5]
+numbers = [14, 121, 34, 14, 55]
 
 def max_number(number):
-  max = 0
+  max = number[0]
   for number in numbers:
     if number > max:
       max = number
+      print(max, number)
     else:
+      print(max, number)
       continue
   return max
 
-print(max_number(numbers))
-    
-  
+print('Maximum number is - ', max_number(numbers))
+
+
+def min_number(number):
+  min = number[0]
+  for number in numbers:
+    if number < min:
+      min = number
+    else:
+      continue
+  return min
+
+print('Minimum number is - ', min_number(numbers))
   
