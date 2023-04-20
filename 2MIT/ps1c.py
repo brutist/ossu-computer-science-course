4090459f31ce389e6f1850db5098ea14ee3e1e42
mauring55 <83412888+mauring55@users.noreply.github.com>
2023-04-20 09:22:19 +0000

    shortened the code a lot. I was stupid yesterday

--- 2MIT/ps1c.py
+++ 2MIT/ps1c.py
@@ -1,89 +1,44 @@
 #Assignment Instruction = https://ocw.mit.edu/courses/6-0001-introduction-to-computer-science-and-programming-in-
 #python-fall-2016/8cf75481d7047180c386de3e485bd050_MIT6_0001F16_ps1.pdf
 
-def bestSavingsRate(salary, portionSaved):
-    #instantiate some values to make the code readable
-    MONTHS_IN_YEAR = 12
-    monthsOfSaving = range(36)#months
-    currentSavings = float(0)
-    monthlyInterestRate = 0.04 / MONTHS_IN_YEAR
-    semiAnnualRaise = 0.07
-    DOWNPAYMENT = 1000000
-    currentSalary = salary
-  
-    for month in monthsOfSaving:
-      #check whether it's time for raise
-      if month % 6 == 0 and month != 0:
-        salaryRaise = currentSalary * semiAnnualRaise
-        currentSalary = currentSalary + salaryRaise
-    
-      #Interest per month should change along with the increase in savings
-      monthlyInterestInSavings = currentSavings * monthlyInterestRate
-      
-      #Add savings 
-      monthlySavings = (currentSalary / MONTHS_IN_YEAR) * portionSaved
-      currentSavings = currentSavings + monthlySavings + monthlyInterestInSavings
-      print(currentSavings)
-      
-      #checking whether 36 months have passed is savings enough
-      if month >= 36:
-        if currentSavings < DOWNPAYMENT: 
-          print('It is not possible to pay the down payment in three years.')
-        else: 
-          print('it is possible')
-
-
-# Prompt the user for the annual salary with error check
 while True:
-  number = input('Enter Annual Salary: ')
   try:
-    annualSalary = float(number)
+    annualSalary = float(input('Enter annual salary here: '))
     break
   except:
-    print('Enter numbers only')
     continue
 
+#semiAnnualRaise = 0.07
+#fullCost = 1000000
+#downPayment = fullCost * 0.25
 
+# This is to store the salary record; 1 entry [difference]
+record = list()
 
-bestSavingsRate(annualSalary, 1)
+def difference(annualSalary, portionSaved, downPayment, semiAnnualRaise):
+  currentSavings = 0
+  month = 0
   
+  while True:
+    
+    if currentSavings >= downPayment:
+      difference = downPayment - currentSavings
+      record.append(difference)
+      print(month)
+      break
+
+    if month % 6 == 0 and month != 0:
+      annualSalary += annualSalary * semiAnnualRaise
+    
+    monthlyReturnOfInvestment = currentSavings * (0.04 / 12)
+    monthlySavings = (annualSalary / 12) * portionSaved
+    currentSavings += monthlySavings + monthlyReturnOfInvestment
+    month += 1
+
+    print('Month: ', month, 'Annual salary:', annualSalary)
+    
   
+DOWNPAYMENT = 800000 * 0.25
+difference(annualSalary, 0.1, DOWNPAYMENT, 0.03)
 
-salaryRecord = list() #This is a list of dictionary with keys "month", "current_savings", "annual_salary"
-record = dict()
-
-
-
-        
-      
-  
-  
-  
-    #This is not really part of the assignment but I really want to do this 
-    #list of record for every month that passed
-    #record["month"] = month
-    #record["current_savings"] = currentSavings#
-    #record['annualSalary'] = currentSalary
-
-    #If you dont cast to list, the entire list will point to the last immutable values it was given with.
-    #making all of the key-value pairs the same in every index of the list
-    # This is not an elegant solution, but using eval() function can turn the list of string into a dictionary again. 
-    #salaryRecord.append(str(record))
-
-#print result
-#print('Number of months:', month)
-
-#Print the salary record for all months 
-#for record in salaryRecord:
-  #print(record)
-
-
-
-
-
-
-
-
-
-
-
+print(record)
