e0aadac2fa0db8700ad5eb9916b7d043907ddc90
Jonathan Mauring Jr <83412888+mauring55@users.noreply.github.com>
2023-05-14 06:02:40 +0000

    experimenting with sorting algos

---
+++ 2MIT/sorting/sort.py
@@ -0,0 +1,21 @@
+# TODO : Implement BOGO sort
+# randomly shuffle the elements in the list until it is sorted
+
+
+
+
+# TODO : Implement BUBBLE sort
+# compare the an L[j] with L[j-1] and swap if L[j] is larger
+# do this swaps until the list is sorted
+
+
+
+
+# TODO : Implement SELECTION sort
+# find the smallest element in the list and put it in index [i]. Continue doing so 
+# with the remaining part of the list until sorted.
+
+
+
+
+# TODO : Implement MERGE sort
