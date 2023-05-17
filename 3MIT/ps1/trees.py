cbc9a42aac17d86b2a5bf17c0220ccf93fbe2551
mauring55 <jonathanmauring1998@gmail.com>
2023-05-17 12:10:37 +0800

    done with simple search trees

--- 3MIT/ps1/trees.py
+++ 3MIT/ps1/trees.py
@@ -1,5 +1,7 @@
 '''
-This file is created to replicate the program in 6.0002 - '2. Optimization Problems'
+This file is created to replicate the program in 6.0002 Introduction to Computational Thinking and Data Science.
+
+All codes here were created from scratch after watching the video '2. Optimization Problems'
 '''
 
 import random
@@ -25,12 +27,6 @@ class Food(object):
 
     def __str__(self):
         return self.name + ' <' + str(self.value) + ', ' + str(self.calorie) + ' calories'+' >'
-
-
-# list of names, values, and calories to initialize the menu
-names = ['donut', 'pork bbq', 'juice', 'fried chicken', 'spaghetti', 'cake', 'rice']
-values = [10, 77, 29, 77, 88, 38, 47]
-calories = [195, 150, 40, 120, 200, 120, 95]
 
 
 # create a list of foods automatically
@@ -70,6 +66,40 @@ def greedy(items, maxCost, keyFunction):
 
     return result, totalValue
 
+def maxVal(toConsider, avail):
+    '''
+    toConsider (list of Food object) - Assumes non-empty list.
+    avail (int) - the available 'space' to optimize picks from toConsider.
+
+    returns a tuple of total value and the list of items of that solution.
+    '''
+    # if the list is empty; total value is 0 and no item were taken (base case)
+    if toConsider == [] or avail == 0:
+        return (0, ())
+    
+    # if taking the particular item would cost greater than what is available,
+    # do not take the item. i.e. go to the left branch of the tree
+    elif toConsider[0].getCost() >= avail:
+        result = maxVal(toConsider[1:], avail)
+    
+    else:
+        # this calculates the value of the right leafs
+        withItem = toConsider[0]
+        withVal, withToTake = maxVal(toConsider[1:], avail - withItem.getCost())
+        withVal += withItem.getValue()
+        
+        # this calculates the value of the left leafs
+        withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
+
+        # find the highest value from the left and right leafs
+        if withVal > withoutVal:
+            result = withVal, withToTake + (withItem,)
+        else:
+            result = withoutVal, withoutToTake
+
+
+    return result
+
 
 def testGreedy(items, constraint, keyFunction):
     taken, val = greedy(items, constraint, keyFunction)
@@ -88,5 +118,30 @@ def testGreedys(maxUnits):
     testGreedy(foods, maxUnits, Food.getDensity)
 
 
+def testMaxVal(maxUnits):
+    print('\nUsing search trees to allocate', maxUnits, 'calories')
+    val, taken = maxVal(foods, maxUnits)
+    print('Total value of items taken:', val)
+    for item in taken:
+        print('   ', item)
+
+# list of names, values, and calories to initialize the menu
+names = ['donut', 'pork bbq', 'juice', 'fried chicken', 'spaghetti', 'cake', 'rice', 'burger', 'lechon']
+values = [10, 77, 29, 77, 88, 38, 47, 57, 79]
+calories = [195, 150, 40, 120, 200, 145, 95, 110, 210]
+
 foods = menuBuilder(names, values, calories)
 testGreedys(800)
+testMaxVal(800)
+
+
+
+# This is the list from the lecture. Copied and tested to see if my algorithm works the same way.
+# Note: Yes. My recreation works the same way!
+names = ['wine', 'beer', 'pizza', 'burger', 'fries', 'cola', 'apple', 'donut', 'cake']
+values = [89,90,95,100,90,79,50,10]
+calories = [123,154,258,354,365,150,95,195]
+foods = menuBuilder(names, values, calories)
+testGreedys(750)
+testMaxVal(750)
+
