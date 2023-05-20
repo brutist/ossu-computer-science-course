6645c9a6cfffc244d2e1e13a7166f9ec3804c442
mauring55 <jonathanmauring1998@gmail.com>
2023-05-20 22:29:15 +0800

    almost done with ps1

--- 3MIT/ps1/ps1b.py
+++ 3MIT/ps1/ps1b.py
@@ -28,19 +28,56 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     # What is the weight limit in this case? What are the values of each item? 
     # What is the weight of each item?
     
-    # this is greedy algorithm which might be an inherently good solution for this problem.
-    #total_weight = 0
-    #egg_taken = []
-    #j = len(egg_weights) - 1
-    #while total_weight < target_weight:
-    #    if not total_weight + egg_weights[j] > target_weight:
-    #        egg_taken.append(egg_weights[j])
-    #        total_weight += egg_weights[j]
-    #    else:
-    #        j -= 1
-    #return len(egg_taken)
+    # recursive dynamic version (top-down version)
     
-    # iterative dynamic version
+    # populate dict with impossible values
+    for j in range(target_weight + 1):
+        memo[j] = target_weight + 1
+
+    # check if answer in memo
+    if target_weight in memo and not memo[target_weight] > target_weight:
+        return memo[target_weight]
+    
+    # base case
+    elif target_weight == 0:
+        return 0
+
+    else:
+        for j in reversed(range(len(egg_weights))):
+            if egg_weights[j] > target_weight:
+                continue
+
+            if egg_weights[j] <= target_weight:
+                temp = 1 + dp_make_weight(egg_weights, target_weight - egg_weights[j], memo)
+
+            if temp < memo[target_weight]:
+                memo[target_weight] = temp
+    print(memo)
+    return memo[target_weight]
+
+
+
+def dp_make_weight_greedy(egg_weights, target_weight, memo = {}):
+    # this is greedy algorithm which might be a good enough solution for this problem.
+    total_weight = 0
+    egg_taken = []
+    j = len(egg_weights) - 1
+    while total_weight < target_weight and j >= 0:
+        if total_weight + egg_weights[j] <= target_weight:
+            egg_taken.append(egg_weights[j])
+            total_weight += egg_weights[j]
+        else:
+            j -= 1
+    
+    if sum(egg_taken) != target_weight:
+        return -1
+    
+    return len(egg_taken)
+
+
+def dp_make_weight_iterative(egg_weights, target_weight, memo = {}):
+    # iterative dynamic version. This ones fucking lovely!
+    
     # populate the memo with an imposible no. of coins
     for j in range(target_weight + 1):
         memo[j] = target_weight + 1
@@ -53,11 +90,18 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
 
         # try all of the possible weights
         for weight in egg_weights:
+            if weight > target: 
+                continue
+
             if weight <= target:
                 temp = 1 + memo[target-weight]
+
             if temp < memo[target]:
                 memo[target] = temp
-
+    
+    if memo[target_weight] >= target_weight + 1:
+        return -1
+    
     return memo[target_weight]
    
 
@@ -68,5 +112,7 @@ if __name__ == '__main__':
     print("Egg weights = (1, 5, 10, 25)")
     print("n = 99")
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
-    print("Actual output:", dp_make_weight(egg_weights, n))
+    print("Actual output GREEDY :", dp_make_weight(egg_weights, n))
+    print("Actual output ITERATIVE:", dp_make_weight_iterative(egg_weights, n))
+    print("Actual output GREEDY :", dp_make_weight_greedy(egg_weights, n))
     print()
