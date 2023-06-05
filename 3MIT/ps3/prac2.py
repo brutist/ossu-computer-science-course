37c7e46f502a1a41e3365dd213dff0862df6f28e
mauring55 <jonathanmauring1998@gmail.com>
2023-06-05 12:18:32 +0800

    still going through lesson codes

--- 3MIT/ps3/prac2.py
+++ 3MIT/ps3/prac2.py
@@ -79,13 +79,102 @@ def same_date(num_people, num_same):
 
     return max(birthdays) >= num_same
 
+
+class FairRoulette(object):
+    def __init__(self):
+        self.pockets = []
+
+        # append all of the possible outcomes in a roulette
+        for i in range(1,37):
+            self.pockets.append(i)
+
+        self.ball = None
+        self.pocket_odds = len(self.pockets) - 1
+
+    def spin(self):
+        self.ball = random.choice(self.pockets)
+
+    def bet_pocket(self, pocket, amount):
+        if self.ball == pocket:
+            return amount*self.pocket_odds
+        else:
+            return -amount
+
+    def __str__(self):
+        return 'Fair Roulette'
+
+class EuropeanRoulette(FairRoulette):
+    def __init__(self):
+        FairRoulette.__init__(self)
+        self.pockets.append('0')
+    def __str__(self):
+        return 'European Roulette'
+
+class AmericanRoulette(EuropeanRoulette):
+    def __init__(self):
+        EuropeanRoulette.__init__(self)
+        self.pockets.append('00')
+    def __str__(self):
+        return 'American Roulette'
+
+def play_roulettte(game, num_spins, pocket, bet, to_print=True):
+    """ Play roulette game num_spins times.
+    :params game (class) - type of roulette to be played
+            num_spins (int) - no. of times to play roulette
+            pocket (int) - pocket to bet on. assumes input is 1-36 only.
+            bet (int) - bet per game
+    
+    return float - average return per game of roulette
+    """
+    total = 0
+    for i in range(num_spins):
+        game.spin()
+        total += game.bet_pocket(pocket, bet)
+
+    if to_print:
+        print(num_spins, 'spins of', game)
+        print('Expected return betting', pocket, '=', str(100*(total/num_spins)), '%')
+        print()
+
+    return total/num_spins
+
+def simulate_games(trials, num_spins, pocket, bet, to_print=True):
+        # This is the different types of roulettes were playing
+    games = [FairRoulette(), EuropeanRoulette(), AmericanRoulette()]
+    
+    for i in range(trials):
+        if to_print:
+            print('Simulate', trials, 'trials of', num_spins, 'spins each.')
+
+        for game in games:
+            exp_return = play_roulettte(game, num_spins, pocket, bet, False)
+
+            if to_print:
+                print('Exp. return for', game, '=', str(100*exp_return), '%')
+        
+        print()
+    
 if __name__ == '__main__':
     GOAL = '1111'
-    TRIALS = 10000
+    TRIALS = 100000
     SAMPLES = 50
     #mean = run_sims(GOAL, TRIALS, SAMPLES, True)
     #print('Estimated probability:', mean)
 
-    PEOPLE = 100
-    SAME = 4
-    print(same_date(PEOPLE, SAME))
+    #PEOPLE = 100
+    #SAME = 4
+    #print(same_date(PEOPLE, SAME))
+
+    #game = FairRoulette()
+    #POCKET = 2
+    #BET = 1
+    #for num_spin in (1, 100, 1000000):
+    #    for i in range(3):
+    #        play_roulettte(game, num_spin, POCKET, BET, True)
+
+    TRIALS = 20
+    NUM_SPINS = 1000000
+    POCKET = 2
+    BET = 1
+    simulate_games(TRIALS, NUM_SPINS, POCKET, BET)
+            
