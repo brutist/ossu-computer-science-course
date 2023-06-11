205b37a4953a9baebc0e8f03d6f22c18aa47744a
mauring55 <jonathanmauring1998@gmail.com>
2023-06-11 16:45:30 +0800

    clarifying what pylab.array does

---
+++ 3MIT/ps5/prac.py
@@ -0,0 +1,12 @@
+import pylab
+
+get_features = pylab.array([89.0, 1.0, 0.0, 66.0])
+get_features2 = pylab.array([100.0, 1.0, 1.0, 70.0])
+examples = [get_features, get_features2]
+
+vals = pylab.array([0.0] * len(examples[0]))
+for e in examples:
+    vals += e
+
+centroid = vals / len(examples)
+print(centroid)
