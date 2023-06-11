f36c1ce731f0463a949f9be7e673d5a189a8477f
mauring55 <jonathanmauring1998@gmail.com>
2023-06-11 21:22:15 +0800

    starting ps5

--- 3MIT/ps5/ps5.py
+++ 3MIT/ps5/ps5.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Problem Set 5: Experimental Analysis
-# Name: 
-# Collaborators (discussion):
-# Time:
+# Name: Jonathan M. Mauring Jr
+# Collaborators (discussion): None
+# Time: start: June 11 end:
 
 import pylab
 import re
