957ae15e30e30e8da535bc6e561585f3883cf4c2
newbz <83412888+mauring55@users.noreply.github.com>
2023-06-17 04:06:35 +0000

    done with puzzle 2 exercise 2

--- 4MIT/puzzle 2/solution.py
+++ 4MIT/puzzle 2/solution.py
@@ -14,36 +14,48 @@ def bestTimeToPartySmart(schedule):
 
     Return best_time (int) - the best hour to party.
     """
-    HOURS_IN_DAY = 24
-    party_hours = [0] * HOURS_IN_DAY
-    
-
     # have a separate list of all start and end times, the hrs_entered correspond to celebrities
     #           coming at that hour, while the hrs_left correspond to celebrities leaving at that hour.
-    start_hour = None
-    end_hour = None
     hrs_entered, hrs_left = [], []
 
     for start, end in schedule:
         hrs_entered.append(start)
         hrs_left.append(end)
 
-        # identify the party start and end time
-        if not start_hour or start < start_hour:
-            start_hour = int(start)
-        if not end_hour or end > end_hour:
-            end_hour = int(end)
+    # round down by int division
+    start_time = min(hrs_entered) // 1
+    end_time = max(hrs_left) // 1
+    
+    # sort list
+    hrs_entered.sort()
+    hrs_left.sort()
 
- 
-    # record how many celebrities present in each hour (24 hr format, hr is the index of the list)
-    current_celebs = 0
-    for h in range(start_hour, end_hour + 1):
-        current_celebs += hrs_entered.count(h) - hrs_left.count(h)
-        party_hours[h] = current_celebs
+    # map the number of celebrities present (value) in each entrance time (key)
+    celebs_attendance = {}
+    for i in range(len(hrs_entered)):
+        current_time = hrs_entered[i]
+        celebs_present = 0
+
+        for h in range(len(hrs_entered)):
+            celeb_start_time = hrs_entered[h]
+            celeb_end_time = hrs_left[h]
+
+            if current_time >= celeb_start_time:
+                celebs_present += 1
+            if current_time >= celeb_end_time:
+                celebs_present -= 1
+    
+        celebs_attendance[hrs_entered[i]] = celebs_present
 
     # identify the hr that has the maximum no. of celebs present
-    best_hr = party_hours.index(max(party_hours))
+    best_hr = None
+    max_present = None
+    for hr, present in celebs_attendance.items():
+        if not max_present or max_present < present:
+            best_hr = hr
+            max_present = present
 
+    print('Best time to attend the party is at {} o\'clock : {} celebrities will be attending!'.format(best_hr, max_present))
     return best_hr
 
 
@@ -57,7 +69,7 @@ if __name__ == '__main__':
           (9, 10), (11, 12), (11, 13), (11, 14)]
 
 
-    print(bestTimeToPartySmart(sched))
-    print(bestTimeToPartySmart(sched2))
-    print(bestTimeToPartySmart (sched3))
+    bestTimeToPartySmart(sched)
+    bestTimeToPartySmart(sched2)
+    bestTimeToPartySmart (sched3)
         
