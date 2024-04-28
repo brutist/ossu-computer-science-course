33ee9a285fd2ac7794e1a22724be2916338f58c7
mauring55 <jonathanmauring1998@gmail.com>
2024-04-28 12:19:11 +0800

    Update prefix-equal in module-8a

--- htc-complex-data/module-8a/prefix-equal-starter.rkt
+++ htc-complex-data/module-8a/prefix-equal-starter.rkt
@@ -2,7 +2,7 @@
 ; if the first list is a prefix of the second. Prefix means that the elements of
 ; the first list match the elements of the second list 1 for 1, and the second list
 ; is at least as long as the first.
-; 
+;
 ; For reference, the ListOfString data definition is provided below.
 
 ;; =================
@@ -18,34 +18,27 @@
 (define LS2 (cons "a" (cons "b" empty)))
 (define LS3 (cons "c" (cons "b" (cons "a" empty))))
 
-#;
-(define (fn-for-los los)
-  (cond [(empty? los) (...)]
-        [else 
-         (... (first los)
-              (fn-for-los (rest los)))]))
-
+#;(define (fn-for-los los)
+    (cond
+      [(empty? los) (...)]
+      [else (... (first los) (fn-for-los (rest los)))]))
 
 ;; Functions
 
 ;; ListOfString ListOfString -> Boolean
 ;; produces true if lsta is a prefix of lstb, otherwise false
 ;; examples/tests
-(check-expect (prefix=? empty      empty)       true)
-(check-expect (prefix=? empty      (list "a"))    true)
-(check-expect (prefix=? (list "a") empty)       false)
-(check-expect (prefix=? (list "a") (list "a"))  true)
-(check-expect (prefix=? (list "a" "b") (list "a" "b" "c"))  true)
-(check-expect (prefix=? (list "a" "c") (list "a" "b" "c"))  false)
-(check-expect (prefix=? (list "a" "b" "c" "d") (list "a" "b" "c"))  false)
-(check-expect (prefix=? (list "a" "b" "d" "d") (list "a" "b" "c"))  false)
-
-
+(check-expect (prefix=? empty empty) true)
+(check-expect (prefix=? empty (list "a")) true)
+(check-expect (prefix=? (list "a") empty) false)
+(check-expect (prefix=? (list "a") (list "a")) true)
+(check-expect (prefix=? (list "a" "b") (list "a" "b" "c")) true)
+(check-expect (prefix=? (list "a" "c") (list "a" "b" "c")) false)
+(check-expect (prefix=? (list "a" "b" "c" "d") (list "a" "b" "c")) false)
+(check-expect (prefix=? (list "a" "b" "d" "d") (list "a" "b" "c")) false)
 
 (define (prefix=? ls1 ls2)
-  (cond [(empty? ls1) true]
-        [(empty? ls2) false]
-        [(string=? (first ls1) (first ls2))
-         (prefix=? (rest ls1) (rest ls2))]
-        [else false]))
-
+  (cond
+    [(empty? ls1) true]
+    [(empty? ls2) false]
+    [else (and (string=? (first ls1) (first ls2)) (prefix=? (rest ls1) (rest ls2)))]))
