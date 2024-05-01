b7ee5f8a2b3c761cc992ed487a035020b9d477ef
mauring55 <jonathanmauring1998@gmail.com>
2024-05-01 10:53:13 +0800

    Solve Problem 1 in abstraction-quiz

--- htc-complex-data/module-9/abstraction.quiz.rkt
+++ htc-complex-data/module-9/abstraction.quiz.rkt
@@ -1,9 +1,28 @@
 (require 2htdp/image)
 ;  PROBLEM 1:
-;  
-;  Design an abstract function called arrange-all to simplify the 
+;
+;  Design an abstract function called arrange-all to simplify the
 ;  above-all and beside-all functions defined below. Rewrite above-all and
 ;  beside-all using your abstract function.
+
+;; abstract function for arranging (listof Image)
+(check-expect (arrange-all overlay
+                           empty-image
+                           (list (regular-polygon 20 5 "solid" (make-color 50 50 255))
+                                 (regular-polygon 26 5 "solid" (make-color 100 100 255))
+                                 (regular-polygon 32 5 "solid" (make-color 150 150 255))
+                                 (regular-polygon 38 5 "solid" (make-color 200 200 255))
+                                 (regular-polygon 44 5 "solid" (make-color 250 250 255))))
+              (overlay (regular-polygon 20 5 "solid" (make-color 50 50 255))
+                       (regular-polygon 26 5 "solid" (make-color 100 100 255))
+                       (regular-polygon 32 5 "solid" (make-color 150 150 255))
+                       (regular-polygon 38 5 "solid" (make-color 200 200 255))
+                       (regular-polygon 44 5 "solid" (make-color 250 250 255))))
+
+(define (arrange-all fn b loi)
+  (cond
+    [(empty? loi) b]
+    [else (fn (first loi) (arrange-all fn b (rest loi)))]))
 
 
 ;; (listof Image) -> Image
@@ -11,44 +30,36 @@
 (check-expect (above-all empty) empty-image)
 (check-expect (above-all (list (rectangle 20 40 "solid" "red") (star 30 "solid" "yellow")))
               (above (rectangle 20 40 "solid" "red") (star 30 "solid" "yellow")))
-(check-expect (above-all (list (circle 30 "outline" "black") (circle 50 "outline" "black") (circle 70 "outline" "black")))
-              (above (circle 30 "outline" "black") (circle 50 "outline" "black") (circle 70 "outline" "black")))
+(check-expect
+ (above-all
+  (list (circle 30 "outline" "black") (circle 50 "outline" "black") (circle 70 "outline" "black")))
+ (above (circle 30 "outline" "black") (circle 50 "outline" "black") (circle 70 "outline" "black")))
 
 ;(define (above-all loi) empty-image)  ;stub
 
-(define (above-all loi)
-  (cond [(empty? loi) empty-image]
-        [else
-         (above (first loi)
-                (above-all (rest loi)))]))
-
-
+(define (above-all loi) (arrange-all above empty-image loi))
 
 ;; (listof Image) -> Image
 ;; combines a list of images into a single image, each image beside the next one
 (check-expect (beside-all empty) (rectangle 0 0 "solid" "white"))
 (check-expect (beside-all (list (rectangle 50 40 "solid" "blue") (triangle 30 "solid" "pink")))
               (beside (rectangle 50 40 "solid" "blue") (triangle 30 "solid" "pink")))
-(check-expect (beside-all (list (circle 10 "outline" "red") (circle 20 "outline" "blue") (circle 10 "outline" "yellow")))
-              (beside (circle 10 "outline" "red") (circle 20 "outline" "blue") (circle 10 "outline" "yellow")))
+(check-expect
+ (beside-all
+  (list (circle 10 "outline" "red") (circle 20 "outline" "blue") (circle 10 "outline" "yellow")))
+ (beside (circle 10 "outline" "red") (circle 20 "outline" "blue") (circle 10 "outline" "yellow")))
 
 ;(define (beside-all loi) empty-image)  ;stub
 
-(define (beside-all loi)
-  (cond [(empty? loi) (rectangle 0 0 "solid" "white")]
-        [else
-         (beside (first loi)
-                 (beside-all (rest loi)))]))
-
+(define (beside-all loi) (arrange-all beside empty-image loi))
 
 
 
 
 ;  PROBLEM 2:
-;  
-;  Finish the design of the following functions, using built-in abstract functions. 
-;  
-
+;
+;  Finish the design of the following functions, using built-in abstract functions.
+;
 
 ;; Function 1
 ;; ==========
@@ -58,9 +69,8 @@
 (check-expect (lengths empty) empty)
 (check-expect (lengths (list "apple" "banana" "pear")) (list 5 6 4))
 
-(define (lengths lst) empty)
-
-
+(define (lengths lst)
+  empty)
 
 ;; Function 2
 ;; ==========
@@ -70,9 +80,8 @@
 (check-expect (odd-only empty) empty)
 (check-expect (odd-only (list 1 2 3 4 5)) (list 1 3 5))
 
-(define (odd-only lon) empty)
-
-
+(define (odd-only lon)
+  empty)
 
 ;; Function 3
 ;; ==========
@@ -83,9 +92,8 @@
 (check-expect (all-odd? (list 1 2 3 4 5)) false)
 (check-expect (all-odd? (list 5 5 79 13)) true)
 
-(define (all-odd? lon) empty)
-
-
+(define (all-odd? lon)
+  empty)
 
 ;; Function 4
 ;; ==========
@@ -96,27 +104,21 @@
 (check-expect (minus-n (list 4 5 6) 1) (list 3 4 5))
 (check-expect (minus-n (list 10 5 7) 4) (list 6 1 3))
 
-(define (minus-n lon n) empty)
-
-
-
-
-
-
+(define (minus-n lon n)
+  empty)
 
 ;  PROBLEM 3
-;  
-;  Consider the data definition below for Region. Design an abstract fold function for region, 
-;  and then use it do design a function that produces a list of all the names of all the 
+;
+;  Consider the data definition below for Region. Design an abstract fold function for region,
+;  and then use it do design a function that produces a list of all the names of all the
 ;  regions in that region.
-;  
+;
 ;  For consistency when answering the multiple choice questions, please order the arguments in your
-;  fold function with combination functions first, then bases, then region. Please number the bases 
+;  fold function with combination functions first, then bases, then region. Please number the bases
 ;  and combination functions in order of where they appear in the function.
-;  
-;  So (all-regions CANADA) would produce 
+;
+;  So (all-regions CANADA) would produce
 ;  (list "Canada" "British Columbia" "Vancouver" "Victoria" "Alberta" "Calgary" "Edmonton")
-
 
 (define-struct region (name type subregions))
 ;; Region is (make-region String Type (listof Region))
@@ -138,29 +140,18 @@
 (define ALBERTA (make-region "Alberta" "Province" (list CALGARY EDMONTON)))
 (define CANADA (make-region "Canada" "Country" (list BC ALBERTA)))
 
-#;
-(define (fn-for-region r)
-  (local [(define (fn-for-region r)
-            (... (region-name r)
-                 (fn-for-type (region-type r))
-                 (fn-for-lor (region-subregions r))))
-          
-          (define (fn-for-type t)
-            (cond [(string=? t "Continent") (...)]
-                  [(string=? t "Country") (...)]
-                  [(string=? t "Province") (...)]
-                  [(string=? t "State") (...)]
-                  [(string=? t "City") (...)]))
-          
-          (define (fn-for-lor lor)
-            (cond [(empty? lor) (...)]
-                  [else 
-                   (... (fn-for-region (first lor))
-                        (fn-for-lor (rest lor)))]))]
-    (fn-for-region r)))
-
-
-
-
-
-                   
+#;(define (fn-for-region r)
+    (local [(define (fn-for-region r)
+              (... (region-name r) (fn-for-type (region-type r)) (fn-for-lor (region-subregions r))))
+            (define (fn-for-type t)
+              (cond
+                [(string=? t "Continent") (...)]
+                [(string=? t "Country") (...)]
+                [(string=? t "Province") (...)]
+                [(string=? t "State") (...)]
+                [(string=? t "City") (...)]))
+            (define (fn-for-lor lor)
+              (cond
+                [(empty? lor) (...)]
+                [else (... (fn-for-region (first lor)) (fn-for-lor (rest lor)))]))]
+           (fn-for-region r)))
