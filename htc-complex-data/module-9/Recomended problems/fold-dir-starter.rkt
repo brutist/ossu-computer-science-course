0bd930db9b42efa7547ce18d858682e715eca6b8
mauring55 <jonathanmauring1998@gmail.com>
2024-05-01 10:07:20 +0800

    Solve fold-dir in module-9

--- htc-complex-data/module-9/Recomended problems/fold-dir-starter.rkt
+++ htc-complex-data/module-9/Recomended problems/fold-dir-starter.rkt
@@ -46,7 +46,7 @@
 (define D5 (make-dir "D5" empty (list I3)))
 (define D6 (make-dir "D6" (list D4 D5) empty))
 (define D7 (make-dir "D7" empty empty))
-(define D8 (make-dir "D8" (list D4 D5) (list D7)))
+(define D8 (make-dir "D8" (list D4 D5) (list I3)))
 (define BLANK (square 0 "solid" "white"))
 
 ;; =================
@@ -56,25 +56,32 @@
 ; Design an abstract fold function for Dir called fold-dir.
 
 ;; examples/tests
-(check-expect (local [(define (list-names n ld li) (append (list n) ld))]
+(check-expect (local [(define (list-names n ld li) (cons n ld))]
                      (fold-dir list-names append above empty BLANK D6))
               (list "D6" "D4" "D5"))
+(check-expect (local [(define (c1 n ld li) (beside ld li))
+                      (define (all-images i li) (beside i li))]
+                     (fold-dir c1 all-images all-images BLANK BLANK D6))
+              (beside BLANK I1 I2 I3))
 
-
-;; (String X Y -> X, Y or Z) (Dir (listof Dir) -> X) (Image (listof Image) -> Y) X Y Dir -> X, Y or Z
+;;         c1            c2             c3      b1   b2       
+;; (String Y Z -> X) (X Y -> Y) (Image Z -> Z)  Y    Z    Dir -> X
 ;; abstract fold function for Directory
-(define (fold-dir fn c1 c2 b1 b2 dir)
-  (fn (dir-name dir)
-      (local [(define (fn-for-lod lod)
-                (cond
-                  [(empty? lod) b1]
-                  [else (c1 (fold-dir fn c1 c2 b1 b2 (first lod)) (fn-for-lod (rest lod)))]))]
-             (fn-for-lod (dir-sub-dirs dir))) ;-> X
-      (local [(define (fn-for-loi loi)
-                (cond
-                  [(empty? loi) b2]
-                  [else (c2 (first loi) (fn-for-loi (rest loi)))]))]
-             (fn-for-loi (dir-images dir))))) ;-> Y
+
+(define (fold-dir c1 c2 c3 b1 b2 d)
+  (local [(define (fn-for-dir d)      ;-> X
+            (c1 (dir-name d) (fn-for-lod (dir-sub-dirs d)) (fn-for-loi (dir-images d))))
+
+          (define (fn-for-lod lod)    ;-> Y
+            (cond
+              [(empty? lod) b1]
+              [else (c2 (fn-for-dir (first lod)) (fn-for-lod (rest lod)))]))
+
+          (define (fn-for-loi loi)    ;-> Z
+            (cond
+              [(empty? loi) b2]
+              [else (c3 (first loi) (fn-for-loi (rest loi)))]))]
+    (fn-for-dir d)))
 
 
 ; PROBLEM B:
@@ -89,12 +96,11 @@
 (check-expect (count-images D4) 2)
 (check-expect (count-images D5) 1)
 (check-expect (count-images D7) 0)
-(check-expect (count-images D8) 3)
+(check-expect (count-images D8) 4)
 
-(define (count-images dir) (local [(define (fn n ld li) (+ li ld))
-                                   (define (c1 d lod) (+ ))
-                                   (define (c2 i loi) (+ 1 loi))]
-                                  (fold-dir fn c1 c2 empty 0 dir))) 
+(define (count-images dir) (local [(define (c1 n ld li) (+ ld li))
+                                   (define (c3 i li) (+ 1 li))]
+                                  (fold-dir c1 + c3 0 0 dir)))
 
 ; PROBLEM C:
 ; Design a function that consumes a Dir and a String. The function looks in
@@ -102,10 +108,24 @@
 ; finds such a directory it should produce true, if not it should produce false.
 ; Use the fold-dir abstract function.
 
+;; Dir String -> Boolean
+;; produce true if the given s is a name of a dir
+;; examples/tests
+(check-expect (search-name D6 "D6") true)
+(check-expect (search-name D7 "D5") false)
+(check-expect (search-name D6 "D4") true)
+(check-expect (search-name D8 "D6") false)
 
-;
+(define (search-name dir s) (local [(define (c1 n ld li) (or (string=? n s) ld))
+                                    (define (c2 d ld)    (or d ld))
+                                    (define (c3 i li)    i)]
+                                    (fold-dir c1 c2 c3 false false dir)))
+
+
 ; PROBLEM D:
-;
 ; Is fold-dir really the best way to code the function from part C? Why or
 ; why not?
-;
+
+;; Judging from the complexity of the fold-dir based search and the existence of andmap, i think 
+;; that fold-dir may not be that efficient. This could be because of the c3's useless existence or 
+;; the complexity may produce more recursion than necessary.
