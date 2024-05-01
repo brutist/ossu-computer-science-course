8c079ae1296dec31f461b1d2c73078deff6ea81b
mauring55 <jonathanmauring1998@gmail.com>
2024-05-01 19:46:22 +0800

    Add termination arguments for fractals

--- htc-complex-data/module-10a/fractals-starter.rkt
+++ htc-complex-data/module-10a/fractals-starter.rkt
@@ -24,6 +24,20 @@
               (above (triangle CUTOFF MODE COLOR)
                      (beside (triangle CUTOFF MODE COLOR) (triangle CUTOFF MODE COLOR))))
 
+;; Problem
+;; Construct a three-part terminationargument for stri.
+
+;; Base case:      (<= s CUTOFF)
+
+;; Reduction step: (/ s RATIO)
+
+;; Argument that repeated application of reduction step will eventually
+;;  reach the base case:
+
+;; As long as the CUTOFF > 0 and RATIO > 1, repeated division of s by RATIO
+;;   will eventually produce <= CUTOFF.
+
+
 (define (stri s)
   (cond
     [(<= s CUTOFF) (triangle s MODE COLOR)]
@@ -46,6 +60,19 @@
         (above (beside sub sub sub)
                (beside sub blk sub)
                (beside sub sub sub))))
+
+;; Problem
+;; Construct a three-part terminationargument for scarp.
+
+;; Base case:      (<= s CUTOFF)
+
+;; Reduction step: (/ s THREE)
+
+;; Argument that repeated application of reduction step will eventually
+;;  reach the base case:
+
+;; As long as the CUTOFF > 0 and THREE > 1, repeated division of s by THREE
+;;   will eventually produce <= CUTOFF.
 
 (define (scarp s)
   (cond
