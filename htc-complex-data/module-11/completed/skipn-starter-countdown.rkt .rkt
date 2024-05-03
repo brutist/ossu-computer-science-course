da63cf68dd932fc1281cea58bca351531ee10e51
mauring55 <jonathanmauring1998@gmail.com>
2024-05-03 23:00:12 +0800

    Add alternative solutions for skipn-starter

--- htc-complex-data/module-11/completed/skipn-starter-countdown.rkt .rkt
+++ htc-complex-data/module-11/completed/skipn-starter-countdown.rkt .rkt
@@ -18,7 +18,7 @@
 (check-expect (skip-n (list "a" "b" "c") 3) (list "a"))
 (check-expect (skip-n (list "a" "b" "c" "d" "e" "f"
                             "g" "h" "i" "j" "k" "l") 2) (list "a" "d" "g" "j"))
-
+#;
 (define (skip-n lox0 n)
     ;; acc: Natural - 1-based index of (first lox) in lox0
     ;;      Natural - 1-based index of the next item to be included in the list
@@ -34,3 +34,42 @@
                                  (skip-n (rest lox) (add1 cn) nn))]))]
     
     (skip-n lox0 1 1)))
+
+
+;; Alternative solution from the lecture video
+;; countdown version
+
+(define (skip-n lox0 n)
+    ;; acc: Natural - countdown from n to 0, zero means element must be included
+
+    ;; (skip-n (list "a" "b" "c" "d" "e" "f") 0)
+    ;; (skip-n (list     "b" "c" "d" "e" "f") 2)     Assume n = 2
+    ;; (skip-n (list         "c" "d" "e" "f") 1)
+    ;; (skip-n (list             "d" "e" "f") 0)
+    ;; (skip-n (list                 "e" "f") 2)
+    (local [(define (skip-n lox acc)
+                (cond [(empty? lox) empty]
+                      [else (if (zero? acc)
+                                 (cons (first lox) (skip-n (rest lox) n))
+                                 (skip-n (rest lox) (sub1 acc)))]))]
+    
+    (skip-n lox0 0)))
+
+;; countoff version
+#;
+(define (skip-n lox0 n)
+    ;; acc: Natural - countoff from 0 to n, zero means element must be included
+   
+    ;; (skip-n (list "a" "b" "c" "d" "e" "f") 0)
+    ;; (skip-n (list     "b" "c" "d" "e" "f") 1)     Assume n = 2
+    ;; (skip-n (list         "c" "d" "e" "f") 2)
+    ;; (skip-n (list             "d" "e" "f") 0)
+    ;; (skip-n (list                 "e" "f") 1)
+    (local [(define (skip-n lox acc)
+                (cond [(empty? lox) empty]
+                      [(= acc 0) (cons (first lox) (skip-n (rest lox) (add1 acc)))]
+                      [else (if (= acc n)
+                                (skip-n (rest lox) 0)
+                                (skip-n (rest lox) (add1 acc)))]))]
+    
+    (skip-n lox0 0)))
