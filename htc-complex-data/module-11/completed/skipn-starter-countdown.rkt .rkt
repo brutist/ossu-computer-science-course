dd044cc8cec6b11015828b05c9564a4cfa65798f
mauring55 <jonathanmauring1998@gmail.com>
2024-05-03 22:27:24 +0800

    Solve skipn-starter

---
+++ htc-complex-data/module-11/completed/skipn-starter-countdown.rkt .rkt
@@ -0,0 +1,36 @@
+;; skipn-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; Design a function that consumes a list of elements lox and a natural number
+; n and produces the list formed by including the first element of lox, then 
+; skipping the next n elements, including an element, skipping the next n 
+; and so on.
+
+;  (skipn (list "a" "b" "c" "d" "e" "f") 2) should produce (list "a" "d")
+
+;; (listof lox) Natural -> (listof lox)
+;; takes in (l1 l2 l3 ...) produces (l1 l1+(n+1) l1+(n+1)+(n+1) ...)
+;; examples/test
+(check-expect (skip-n empty 1) empty)
+(check-expect (skip-n (list "a" "b" "c" "d" "e" "f") 2) (list "a" "d"))
+(check-expect (skip-n (list "a" "b" "c") 3) (list "a"))
+(check-expect (skip-n (list "a" "b" "c" "d" "e" "f"
+                            "g" "h" "i" "j" "k" "l") 2) (list "a" "d" "g" "j"))
+
+(define (skip-n lox0 n)
+    ;; acc: Natural - 1-based index of (first lox) in lox0
+    ;;      Natural - 1-based index of the next item to be included in the list
+    ;; (skip-n (list "a" "b" "c" "d" "e" "f") 1 1)
+    ;; (skip-n (list     "b" "c" "d" "e" "f") 2 4)     Assume n = 2
+    ;; (skip-n (list         "c" "d" "e" "f") 3 4)
+    ;; (skip-n (list             "d" "e" "f") 4 4)
+    ;; (skip-n (list                 "e" "f") 5 7)
+    (local [(define (skip-n lox cn nn)
+                (cond [(empty? lox) empty]
+                      [else (if (= cn nn)
+                                 (cons (first lox) (skip-n (rest lox) (add1 cn) (+ nn (+ n 1))))
+                                 (skip-n (rest lox) (add1 cn) nn))]))]
+    
+    (skip-n lox0 1 1)))
