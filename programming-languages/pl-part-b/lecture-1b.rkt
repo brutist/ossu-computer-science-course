c630eeb20f11c14421fd5841a8d7e4916d4a8d7f
mauring55 <jonathanmauring1998@gmail.com>
2024-05-23 21:07:18 +0800

    Save lecture codes on memoization

--- programming-languages/pl-part-b/lecture-1b.rkt
+++ programming-languages/pl-part-b/lecture-1b.rkt
@@ -4,18 +4,6 @@
 ;; lazy evaluation is deferring an expensive calculation,
 ;;   but when you do calculate it, you remember it so
 ;;   that you don't have to do it again
-(define (fib n)
-    (local [(define (fib_0 n n1 n2)
-                (cond [(= n 0) 0]
-                      [(= n 1) (+ n1 n2)]
-                      [else (fib_0 (- n 1) (+ n1 n2) n1)]))]
-        (fib_0 n 0 1)))
-
-(define (fib-slow n)
-    (cond [(= n 0) 0]
-          [(= n 1) 1]
-          [else (+ (fib-slow (- n 1)) (fib-slow (- n 2)))]))
-
 
 (define (mults x y-thunk)
     (cond [(= x 0) 0]
@@ -83,3 +71,46 @@
 ;; implement nats and powers-of-two using abstract function
 (define nats2 (stream-maker + 1))
 (define powers-of-two2 (stream-maker * 2)) 
+
+
+;; memoization is keeping a record of past results as a way
+;;  to optimize the calculation. It is more powerful than 
+;;  force and delay due to multiple results from different 
+;;  arguments can be stored by a memo
+
+
+;; let's take a look at a naive solution for nth fibonacci 
+
+;; due to multiple recursions in the result of the function,
+;;  the calls increases exponentially as n increases
+(define (fib-slow n)
+    (cond [(= n 0) 0]
+          [(= n 1) 1]
+          [else (+ (fib-slow (- n 1)) (fib-slow (- n 2)))]))
+
+;; we can simplify and optimize nth fibonacci by keeping track
+;;  of the elements right before and 2 places before the nth element,
+;;  since the nth element is just (n-1) + (n-2), the resulting answer
+;;  once we reach the end of the count will be (n-1) + (n-2)
+(define (fib n)
+    (local [(define (fib_0 n n1 n2)
+                (cond [(= n 0) 0]
+                      [(= n 1) (+ n1 n2)]
+                      [else (fib_0 (- n 1) (+ n1 n2) n1)]))]
+        (fib_0 n 0 1)))
+
+
+;; however, there is an alternative solution that would use a memo
+;;  to keep track of the answers for every calculated fib(x), so that 
+;;  even if the recursion calls are still growing, there is no need
+;;  to recalculate. We can just look at the memo and directly get the 
+;;  answer.
+
+(define (fib-memo n)
+    (local [(define (val-in-memo k m) (cdr (assoc k m)))
+            (define (add-memo-vals n1 n2 memo) (+ (val-in-memo n1 memo) (val-in-memo n2 memo)))
+            (define (aux c memo)
+                (cond [(> c n) (val-in-memo n memo)]
+                      [(or (= c 1) (= c 2)) (aux (+ c 1) (cons (cons c 1) memo))]
+                      [else (aux (+ c 1) (cons (cons c (add-memo-vals (- c 1) (- c 2) memo)) memo))]))]
+    (aux 0 null)))
