3f2883dd24b7d287e6f68fd4959bc20edb07a79d
mauring55 <jonathanmauring1998@gmail.com>
2024-05-24 10:35:34 +0800

    Save work on hw4

--- programming-languages/pl-part-b/hw4-test.rkt
+++ programming-languages/pl-part-b/hw4-test.rkt
@@ -7,10 +7,14 @@
 ;; Uncomment the line below and change HOMEWORK_FILE to the name of your homework file.
 ;;(require "HOMEWORK_FILE")
 
+(require "hw4.rkt")
 (require rackunit)
 
 ;; Helper functions
 (define ones (lambda () (cons 1 ones)))
+(define powers-of-two
+  (local [(define (f x) (cons x (lambda () (f (* x 2)))))]
+    (lambda () (f 2))))
 (define a 2)
 
 (define tests
@@ -18,41 +22,36 @@
    "Sample tests for Assignment 4"
    
    ; sequence test
-   (check-equal? (sequence 0 5 1) (list 0 1 2 3 4 5) "Sequence test")
+   (check-equal? (sequence 0 5 1) (list 0 1 2 3 4 5) "Sequence test a")
+   (check-equal? (sequence 3 11 2) (list 3 5 7 9 11) "Sequence test b")
+   (check-equal? (sequence 3 8 3) (list 3 6) "Sequence test c")
+   (check-equal? (sequence 3 2 1) (list) "Sequence test d")
 
    ; string-append-map test
    (check-equal? (string-append-map 
                   (list "dan" "dog" "curry" "dog2") 
-                  ".jpg") '("dan.jpg" "dog.jpg" "curry.jpg" "dog2.jpg") "string-append-map test")
-   
+                  ".jpg") '("dan.jpg" "dog.jpg" "curry.jpg" "dog2.jpg") "string-append-map test a")
+   (check-equal? (string-append-map 
+                  (list "dan" "dog" "curry" "dog2") 
+                  ".rkt") '("dan.rkt" "dog.rkt" "curry.rkt" "dog2.rkt") "string-append-map test b")
+
    ; list-nth-mod test
-   (check-equal? (list-nth-mod (list 0 1 2 3 4) 2) 2 "list-nth-mod test")
-   
+   (check-equal? (list-nth-mod (list 0 1 2 3 4) 2) 2 "list-nth-mod test a")
+   (check-equal? (list-nth-mod (list 0 1 2 8 4 6 8) 3) 8 "list-nth-mod test b")
+   (check-exn exn:fail? (lambda () (list-nth-mod (list) 3)) "list-nth-mod: empty list")
+   (check-exn exn:fail? (lambda () (list-nth-mod (list 1 2 2) -3)) "list-nth-mod: negative number")
+
    ; stream-for-n-steps test
-   (check-equal? (stream-for-n-steps ones 2) (list 1 1) "stream-for-n-steps test")
-   
+   (check-equal? (stream-for-n-steps ones 2) (list 1 1) "stream-for-n-steps test a")
+   (check-equal? (stream-for-n-steps powers-of-two 3) (list 2 4 8) "stream-for-n-steps test b")
+   (check-equal? (stream-for-n-steps powers-of-two 6) (list 2 4 8 16 32 64) "stream-for-n-steps test b")
+
    ; funny-number-stream test
-   (check-equal? (stream-for-n-steps funny-number-stream 16) (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16) "funny-number-stream test")
-   
-   ; dan-then-dog test
-   (check-equal? (stream-for-n-steps dan-then-dog 1) (list "dan.jpg") "dan-then-dog test")
-   
-   ; stream-add-zero test
-   (check-equal? (stream-for-n-steps (stream-add-zero ones) 1) (list (cons 0 1)) "stream-add-zero test")
-   
-   ; cycle-lists test
-   (check-equal? (stream-for-n-steps (cycle-lists (list 1 2 3) (list "a" "b")) 3) (list (cons 1 "a") (cons 2 "b") (cons 3 "a")) 
-                 "cycle-lists test")
-   
-   ; vector-assoc test
-   (check-equal? (vector-assoc 4 (vector (cons 2 1) (cons 3 1) (cons 4 1) (cons 5 1))) (cons 4 1) "vector-assoc test")
-   
-   ; cached-assoc tests
-   (check-equal? ((cached-assoc (list (cons 1 2) (cons 3 4)) 3) 3) (cons 3 4) "cached-assoc test")
-   
-   ; while-less test
-   (check-equal? (while-less 7 do (begin (set! a (+ a 1)) a)) #t "while-less test")
-   
+   (check-equal? (stream-for-n-steps funny-number-stream 16) (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16) "funny-number-stream test a")
+   (check-equal? (stream-for-n-steps funny-number-stream 20)
+                (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16 17 18 19 -20)  "funny-number-stream test a")
+
+
    ))
 
 (require rackunit/text-ui)
--- programming-languages/pl-part-b/hw4.rkt
+++ programming-languages/pl-part-b/hw4.rkt
@@ -2,3 +2,52 @@
 #lang racket 
 ;; make functions/bindings in this file externally available
 (provide (all-defined-out))
+
+
+;; Int Int Natural -> (listof Int)
+;; produces a list of int starting from low, suceeding elements
+;;   will then be the incremented with stride until high
+(define (sequence low high stride)
+    (local [(define (aux n rsf)
+                (if (> n high)
+                    rsf
+                    (aux (+ n stride) (append rsf (list n)))))]
+        (aux low null)))
+
+
+;; (listof String) String -> (listof String)
+;; produce a new list with each element appended s
+(define (string-append-map xs s)
+    (map (lambda (x) (string-append x s)) xs))
+
+
+;; (listof xs) Int -> x
+;; produce the ith element (0-based index) of the list, i is the remainder produced
+;;  when dividing n by the list’s length
+(define (list-nth-mod xs n)
+    (local [(define i (remainder n (length xs)))]
+    (cond [(null? xs) (error "list-nth-mod: empty list")]
+          [(< n 0) (error "list-nth-mod: negative number")]
+          [else (car (list-tail xs i))])))
+
+
+;; stream is represented as (cons x . next-stream)
+;; Stream Int -> (listof x)
+;; produces the first nth elements in the stream
+(define (stream-for-n-steps stream n)
+    (local [(define (aux stream p rsf) 
+            (local [(define val (car (stream)))
+                    (define next-stream (cdr (stream)))]
+                (cond [(>= p n) rsf]
+                      [else (aux next-stream (+ p 1) (append rsf (list val)))])))]
+        (aux stream 0 null)))
+
+
+;; a stream of natural numbers starting from 1 but all numbers divisible by 5
+;;  are negated
+(define funny-number-stream
+    (local [(define (f x) (cons (if (= (modulo x 5) 0) (- x) x) (lambda () (f (+ (abs x) 1)))))]
+        (lambda () (f 1))))
+
+
+;; a stream of alternating strings of "dan.jpg" and "dog.jpg"
