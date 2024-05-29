0f788b508f7516482ab4b3510e65c974b31f6f55
mauring55 <jonathanmauring1998@gmail.com>
2024-05-30 00:06:37 +0800

    Save lecture code

--- programming-languages/pl-part-c/lecture-1b.rb
+++ programming-languages/pl-part-c/lecture-1b.rb
@@ -11,3 +11,16 @@
 
 i = 5
 [4,5,6].each {|x| if i < x then puts (x+1) end}
+
+a = Array.new(5) {|i| (1+i)}
+
+# an example of how to use blocks for a loop
+class Tri
+    def t i
+        (0..i).each do |j|
+            print "  " * j
+            (j..i).each {|k| print k; print " "}
+            print "\n"
+        end
+    end
+end
