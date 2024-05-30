8e3fa27d3bf74754ef1b7808cc7946fc92676bdf
mauring55 <jonathanmauring1998@gmail.com>
2024-05-31 00:21:01 +0800

    Save lecture code

--- programming-languages/pl-part-c/lecture-1b.rb
+++ programming-languages/pl-part-c/lecture-1b.rb
@@ -37,4 +37,90 @@ h1["a"] = "Found a"
 h1.keys
 h1.values
 
+h2 = {"SML"=>1, "Racket"=>2, "Ruby"=>3}
+h2["SML"]
+h2.size
 
+
+class Point
+    attr_accessor :x, :y     # defines methods x, y, x=, y=
+
+    def initialize(x,y)
+        @x = x
+        @y = y
+    end
+    def distFromOrigin
+        Math.sqrt(@x * @x + @y * @y) # uses instance variables
+    end
+    def distFromOrigin2
+        Math.sqrt(x * x + y * y) # uses getter methods
+    end
+end
+
+class ColorPoint < Point
+    attr_accessor :color # color 
+
+    def initialize(x,y,c="clear")
+        super(x,y)       # keyword super calls same method in superclass
+        @color = c
+    end
+end
+
+
+class ColorPoint2 
+    attr_accessor :color
+    def initialize(x,y,c="clear")
+        @pt = Point.new(x,y)
+        @color = c
+    end
+    def x
+        @pt.x
+    end
+    def y
+        @pt.y
+    end
+end
+
+class ThreeDPoint < Point
+    attr_accessor :z
+    def initialize(x,y,z)
+        super(x,y)
+        @z = z
+    end
+    def distFromOrigin
+        d = super
+        Math.sqrt(d * d + @z * @z)
+    end
+    def distFromOrigin2
+        d = super
+        Math.sqrt(d * d + z * z)
+    end
+end
+
+class PolarPoint < Point
+    def initialize(r,theta)
+        @r = r
+        @theta = theta
+    end
+    def x 
+        @r * Math.cos(@theta)
+    end
+    def y
+        @r * Math.sin(@theta)
+    end
+    def x= a
+        b = y
+        @theta = Math.atan2(b,a)
+        @r = Math.sqrt(a*a + b*b)
+        self
+    end
+    def y= a
+        a = x
+        @theta = Math.atan2(b,a)
+        @r = Math.sqrt(a*a + b*b)
+        self
+    end
+    def distFromOrigin
+        @r
+    end
+end
