d06787e8a0f6795f600ff7cf70b16aaea011f760
brutist <jonathanmauring1998@gmail.com>
2024-06-05 09:57:07 +0800

    Initial commit for oop-specialization

---
+++ oop-in-java-specialization-ucsd/hello.java
@@ -0,0 +1,15 @@
+import edu.duke.FileResource;
+
+public class hello {
+    public void runHello () {
+        FileResource res = new FileResource("hello_unicode.txt");
+        for (String line : res.lines()) {
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args){
+        hello hw = new hello();
+        hw.runHello();
+    }
+}
