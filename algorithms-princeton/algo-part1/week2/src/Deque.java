94f3bcc21ce72fe4d8eb0bbba31f6cdeca730e18
brutist <jonathanmauring1998@gmail.com>
2024-06-24 01:18:55 +0800

    Add main method unit tests for Deque class

--- algorithms-princeton/algo-part1/week2/src/Deque.java
+++ algorithms-princeton/algo-part1/week2/src/Deque.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 public class Deque<Item> implements Iterable<Item> {
     int size;
     Node lastNode, firstNode;
+
     private class Node {
         Item item;
         Node prev, next;
@@ -38,6 +39,13 @@ public class Deque<Item> implements Iterable<Item> {
         // set the new node to be the new first
         newFirst.prev = firstNode;
 
+        if (!isEmpty()) {
+            firstNode.next = newFirst;
+        }
+        else {
+            lastNode = newFirst;
+        }
+
         firstNode = newFirst;
         size++;
 
@@ -58,12 +66,15 @@ public class Deque<Item> implements Iterable<Item> {
         newLast.next = lastNode;
         newLast.prev = null;
 
+        if (!isEmpty()) {
+            lastNode.prev = newLast;
+        }
+        else {
+            firstNode = newLast;
+        }
+
         lastNode = newLast;
         size++;
-
-        if (size == 1) {
-            firstNode = lastNode;
-        }
     }
 
     // remove and return the item from the front
@@ -71,7 +82,6 @@ public class Deque<Item> implements Iterable<Item> {
         if (isEmpty()) {
             throw new NoSuchElementException("deque is empty");
         }
-
         Item query = firstNode.item;
 
         if (size == 1) {
@@ -116,10 +126,11 @@ public class Deque<Item> implements Iterable<Item> {
 
     // implements iterator
     private class DequeIterator implements Iterator<Item> {
+        Node currentNode = firstNode;
 
         @Override
         public boolean hasNext() {
-            return !isEmpty();
+            return currentNode != null;
         }
 
         @Override
@@ -127,9 +138,8 @@ public class Deque<Item> implements Iterable<Item> {
             if (!hasNext()) {
                 throw new NoSuchElementException("no more elements in the iterator");
             }
-            Node currentNode = firstNode;
             Item query = currentNode.item;
-            currentNode = currentNode.next;
+            currentNode = currentNode.prev;
             return query;
         }
 
@@ -154,16 +164,12 @@ public class Deque<Item> implements Iterable<Item> {
             if (dequeA.size() != i + 1) {
                 resultA = false;
             }
-            if (i == 1 || i == 2) {
-                dequeA.removeFirst();
-            }
-
-            if (dequeA.size() != inputsA.length - 2) {
-                resultA = false;
-            }
         }
+        for (; i > 0; i--) {
+            dequeA.removeFirst();
+        }
+        if (resultA && dequeA.isEmpty()) { System.out.printf("isEmpty() and size() %d tests passed\n", i + 1); }
 
-        if (resultA ) { System.out.printf("isEmpty() and size() %d tests passed\n", i + 1); }
 
         // test addLast() and removeFirst()
         boolean resultB = true;
@@ -171,20 +177,135 @@ public class Deque<Item> implements Iterable<Item> {
         i = 0;
         for (; i < inputsA.length; i++) {
             dequeB.addLast(inputsA[i]);
-            //System.out.printf("%s  => ", dequeB.removeFirst());
-            //dequeB.addLast(inputsA[i]);
             if (dequeB.isEmpty()) {
                 resultB = false;
                 System.out.print("test addLast() and removeFirst(): deque should not be empty");
             }
-        }
-        for (String s : inputsA) {
             String first = dequeB.removeFirst();
-            if (!first.equals(s)) {
+            if (!first.equals(inputsA[i])) {
                 resultB = false;
                 System.out.print("test addLast() and removeFirst(): removeFirst() is getting wrong answers");
             }
         }
-        if (resultB) { System.out.printf("addLast() and removeFirst() %d tests passed\n", i + 1); }
+        if (resultB) { System.out.printf("addLast() and removeFirst() %d tests passed\n", i); }
+
+
+        // test addFirst() and removeLast()
+        Deque<String> dequeC = new Deque<String>();
+        for (String s : inputsA) {
+            dequeC.addFirst(s);
+        }
+        i = 0;
+        boolean resultC = true;
+        for (; i < inputsA.length; i++) {
+            String ans = dequeC.removeLast();
+            if (!ans.equals(inputsA[i])) {
+                resultC = false;
+                System.out.printf("test addFirst() and removeLast(): got '%s' instead of '%s'\n", ans, inputsA[i]);
+            }
+        }
+        if (resultC) { System.out.printf("addFirst() and removeLast() %d tests passed\n", i); }
+
+
+        // test addLast() and removeLast()
+        Deque<String> dequeD = new Deque<String>();
+        for (String s : inputsA) {
+            dequeD.addLast(s);
+        }
+        i = inputsA.length - 1;
+        boolean resultD = true;
+        for (; i >= 0; i--) {
+            String ans = dequeD.removeLast();
+            if (!ans.equals(inputsA[i])) {
+                resultD = false;
+                System.out.printf("test addLast() and removeLast(): got '%s' instead of '%s'\n", ans, inputsA[i]);
+            }
+        }
+        if (resultD) { System.out.printf("addLast() and removeLast() %d tests passed\n", inputsA.length - 1 - i); }
+
+
+
+        // test addFirst() and removeFirst()
+        Deque<String> dequeE = new Deque<String>();
+        for (String s : inputsA) {
+            dequeE.addFirst(s);
+        }
+        i = inputsA.length  - 1;
+        boolean resultE = true;
+        for (; i >= 0; i--) {
+            String ans = dequeE.removeFirst();
+            if (!ans.equals(inputsA[i])) {
+                resultE = false;
+                System.out.printf("test addFirst() and removeFirst(): got '%s' instead of '%s'\n", ans, inputsA[i]);
+            }
+        }
+        if (resultE) { System.out.printf("addFirst() and removeFirst() %d tests passed\n", inputsA.length - 1 - i); }
+
+
+        // test iterator()
+        Deque<String> dequeF = new Deque<String>();
+        for (String string : inputsA) {
+            dequeF.addLast(string);
+        }
+        // test for iterator
+        i = 0;
+        boolean resultF = true;
+        for (String s : dequeF) {
+            if (!s.equals(inputsA[i])) {
+                resultF = false;
+                System.out.printf("test iterator(): got '%s' instead of '%s'\n", s, inputsA[i]);
+            }
+            i++;
+        }
+        if (resultF && i == inputsA.length) { System.out.printf("iterator() %d tests passed\n", i); }
+
+        // test corner cases
+        // null arguments for addFirst() or addLast() throw IllegalArgumentException
+        Deque<String> dequeG = new Deque<String>();
+        try {
+            dequeG.addFirst(null);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("addFirst() null argument test passed");
+        }
+
+        try {
+            dequeG.addLast(null);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("addLast() null argument test passed");
+        }
+
+        // removeFirst() or removeLast() when the deque is empty throw NoSuchElementException
+        try {
+            dequeG.removeFirst();
+        }
+        catch (NoSuchElementException e) {
+            System.out.println("removeFirst() empty deque test passed");
+        }
+
+        try {
+            dequeG.removeLast();
+        }
+        catch (NoSuchElementException e) {
+            System.out.println("removeLast() empty deque test passed");
+        }
+
+        // next() no more items to return throw NoSuchElementException
+        try {
+            dequeG.iterator().next();
+        }
+        catch (NoSuchElementException e) {
+            System.out.println("iterator().next() empty deque test passed");
+        }
+
+        // remove() throw UnsupportedOperationException
+        try {
+            dequeG.addFirst("test");
+            dequeG.iterator().remove();
+        }
+        catch (UnsupportedOperationException e) {
+            System.out.println("iterator().remove() empty deque test passed");
+        }
     }
 }
