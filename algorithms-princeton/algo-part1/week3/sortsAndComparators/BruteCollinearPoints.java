30eecec1c6393340166de45cec5ae27da464338b
brutist <jonathanmauring1998@gmail.com>
2024-06-28 15:24:55 +0800

    Make BruteCollinearPoints more performant
    Move all method calls to the highest loop possible and add checks
    in each loops to continue the loop if conditions for collinearity
    are not satisfied within the loop

--- algorithms-princeton/algo-part1/week3/sortsAndComparators/BruteCollinearPoints.java
+++ algorithms-princeton/algo-part1/week3/sortsAndComparators/BruteCollinearPoints.java
@@ -35,9 +35,26 @@ public class BruteCollinearPoints {
                 if (points[i].compareTo(points[j]) == 0) {
                     throw new IllegalArgumentException("Point[] should not contain repeated points");
                 }
+                // points[i] must be the lowest to avoid duplicates
+                int ij = points[i].compareTo(points[j]);
+                if (ij >= 0) {
+                    continue;
+                }
+
+                // calculate the slope that points[j] makes with points[i]
+                Double jSlope = points[i].slopeTo(points[j]);
 
                 for (int k = 0; k < N; k++) {
                     if (k == j || k == i) {
+                        continue;
+                    }
+                    // calculate the slope that points[k] makes with points[i]
+                    Double kSlope = points[i].slopeTo(points[k]);
+                    boolean equalJKSlope = jSlope.equals(kSlope);
+                    int jk = points[j].compareTo(points[k]);
+
+                    // no need to check other points if not equal
+                    if (!equalJKSlope || jk >= 0) {
                         continue;
                     }
 
@@ -46,12 +63,10 @@ public class BruteCollinearPoints {
                             continue;
                         }
 
-                        Double jSlope = points[i].slopeTo(points[j]);
-                        Double kSlope = points[i].slopeTo(points[k]);
                         Double mSlope = points[i].slopeTo(points[m]);
                         // a line segment is formed when the slope formed by Point p to
                         //   other Points q, r, s are equal
-                        boolean lineSegFormed = jSlope.equals(kSlope) && jSlope.equals(mSlope);
+                        boolean lineSegFormed = jSlope.equals(mSlope);
                         // true if we have already found a line whose endpoints are i and m
                         boolean endPointsIncluded = endPoints[i] == m || endPoints[m] == i;
 
@@ -59,15 +74,9 @@ public class BruteCollinearPoints {
                         //   a segment is valid if it is either decreasing or increasing
                         //   i.e. if a segment p -> q -> r -> s was found, make sure it is impossible
                         //        to mistakenly add q -> p -> r -> s (not a valid segment)
-                        int ij = points[i].compareTo(points[j]);
-                        int jk = points[j].compareTo(points[k]);
-                        int km = points[k].compareTo(points[m]);
-
                         // vertical and horizontal segments are also increasing or decreasing segments
                         //  no need equal points check because equal points are not allowed
-                        boolean increasingSegment = ij < 0 && jk < 0 && km < 0;
-                        boolean decreasingSegment = ij > 0 && jk > 0 && km > 0;
-                        boolean validSegment = increasingSegment || decreasingSegment;
+                        boolean validSegment = points[k].compareTo(points[m]) < 0;
 
                         if (lineSegFormed && validSegment && (!endPointsIncluded)) {
                             if (endPoints[i] != m) {
@@ -119,37 +128,19 @@ public class BruteCollinearPoints {
         In in = new In(args[0]);
         int n = in.readInt();
         Point[] points = new Point[n];
-        int maxX = 0;
-        int maxY = 0;
         for (int i = 0; i < n; i++) {
             int x = in.readInt();
             int y = in.readInt();
-
-            if (Math.abs(x) > maxX) {
-                maxX = Math.abs(x);
-            }
-            if (Math.abs(y) > maxY) {
-                maxY = Math.abs(y);
-            }
-
             points[i] = new Point(x, y);
         }
 
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, maxX);
-        StdDraw.setYscale(0, maxX);
-        for (Point p : points) {
-            p.draw();
+        BruteCollinearPoints bruteforce = new BruteCollinearPoints(points);
+        LineSegment[] segments = bruteforce.segments();
+        System.out.printf("Segments found: %s\n", segments.length);
+        for (LineSegment s : segments) {
+            System.out.println(s);
         }
-        StdDraw.show();
 
-        // print and draw the line segments
-        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
-        for (LineSegment segment : collinear.segments()) {
-            StdOut.println(segment);
-            segment.draw();
-        }
-        StdDraw.show();
+
     }
 }
