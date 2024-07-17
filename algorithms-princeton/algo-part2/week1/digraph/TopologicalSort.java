135da73cb1e7f24798264560c886e4ca49f9cba0
brutist <jonathanmauring1998@gmail.com>
2024-07-17 20:28:42 +0800

    Add TopologicalSort class

---
+++ algorithms-princeton/algo-part2/week1/digraph/TopologicalSort.java
@@ -0,0 +1,29 @@
+import edu.princeton.cs.algs4.Stack;
+
+public class TopologicalSort {
+    private final boolean[] visited;
+    private final Stack<Integer> reversePost;
+
+    public TopologicalSort(Graph G) {
+        reversePost = new Stack<>();
+        visited = new boolean[G.V()];
+
+        for (int v = 0; v < G.V(); v++) {
+            if (!visited[v]) {
+                dfs(G, v);
+            }
+        }
+    }
+
+    private void dfs(Graph G, int v) {
+        visited[v] = true;
+        for (int w : G.adj(v))
+            if (!visited[w])    dfs(G, w);
+
+        reversePost.push(v);
+    }
+
+    public Iterable<Integer> reversePost() {
+        return reversePost;
+    }
+}
