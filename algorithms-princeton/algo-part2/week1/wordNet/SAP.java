14db426fa2beef3ebde9159e132c1151d09afade
brutist <jonathanmauring1998@gmail.com>
2024-07-17 21:37:50 +0800

    Add test client for SAP

--- algorithms-princeton/algo-part2/week1/wordNet/SAP.java
+++ algorithms-princeton/algo-part2/week1/wordNet/SAP.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class SAP {
 
     // constructor takes a digraph (not necessarily a DAG)
@@ -27,6 +31,15 @@ public class SAP {
 
     // do unit testing of this class
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length   = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
     }
 }
