e5ccfc9d4093f5e7bec9bfdab2a6b3fdbe49d75b
brutist <jonathanmauring1998@gmail.com>
2024-07-31 22:33:21 +0800

    Add iterator for keys in TST class

--- algorithms-princeton/algo-part2/week4/trie/TernarySearchTree.java
+++ algorithms-princeton/algo-part2/week4/trie/TernarySearchTree.java
@@ -1,4 +1,9 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.ArrayDeque;
+
 public class TernarySearchTree<Value> {
+    private int n;
     private Node root = new Node();
 
     private class Node {
@@ -9,6 +14,7 @@ public class TernarySearchTree<Value> {
 
     public void put(String key, Value val) {
         root = put(root, key, val, 0);
+        n++;
     }
 
     private Node put(Node x, String key, Value val, int d) {
@@ -46,5 +52,44 @@ public class TernarySearchTree<Value> {
         else if (c > x.ch)                  return get(x.right, key, d);
         else if (d < key.length() - 1)      return get(x.middle, key, d + 1);
         else                                return x;
+    }
+
+    public Iterable<String> keys() {
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        collect(root, "", queue);
+        return queue;
+    }
+
+    private void collect(Node x, String prefix, ArrayDeque<String> q) {
+        if (x == null)      return;
+        if (x.val != null)  q.addLast(prefix + x.ch);   // add last character
+
+        collect(x.left, prefix, q);
+        collect(x.middle, prefix + x.ch, q);
+        collect(x.right, prefix, q);
+    }
+
+    public int size() {
+        return n;
+    }
+
+
+    public static void main(String[] args) {
+
+        // build symbol table from standard input
+        TernarySearchTree<Integer> st = new TernarySearchTree<Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+
+        // print results
+        if (st.size() < 100) {
+            StdOut.println("keys(\"\"):");
+            for (String key : st.keys()) {
+                StdOut.println(key + " " + st.get(key));
+            }
+            StdOut.println();
+        }
     }
 }
