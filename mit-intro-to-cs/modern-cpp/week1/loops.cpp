4c13401737f7fb6555b3499ca6f0576514a07abc
brutist <jmmauring@up.edu.ph>
2024-08-15 16:34:16 +0800

    Save work on week1 modern c++

---
+++ mit-intro-to-cs/modern-cpp/week1/loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char const *argv[]) {
+    const int counter = 10;
+    std::vector<int> vec = {1, 2, 4, 4, 5};
+    for (int i = 0; i < vec.size(); i++)
+        std::cout << vec[i] << std::endl;
+
+    for (int i : vec)
+        std::cout << i << std::endl;
+}
---
+++ mit-intro-to-cs/modern-cpp/week1/vector.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::vector<int> vec = {1, 3, 2, 4};
+    std::cout << vec.front() << " " << vec.back() << std::endl;
+    return 0;
+}
