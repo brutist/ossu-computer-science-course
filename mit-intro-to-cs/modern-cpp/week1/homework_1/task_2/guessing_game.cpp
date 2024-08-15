954582239621ea4774bf0540229a35ad923be9ce
brutist <jonathanmauring1998@gmail.com>
2024-08-15 22:55:49 +0800

    Complete week1 homework of modern c++

---
+++ mit-intro-to-cs/modern-cpp/week1/homework_1/task_2/guessing_game.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+
+// pick a random number from 0-99 
+// guess the number by stdin
+//  if guess is correct => You have won
+//  if guess < target   => guess is smaller
+//  if guess > target   => guess is larger
+int main() {
+    // Start message
+    std::cout << "Guess the number from 0 - 99" << std::endl;
+
+    srand(time(NULL));
+    int random_num = std::rand() % 100;
+    bool guessed = false;
+    while (!guessed) {
+        int guess;
+        std::cin >> guess;
+
+        if (guess == random_num) {
+            std::cout << "You have won!" << std::endl;
+            guessed = true;
+        }
+        else if (guess < random_num) {
+            std::cout << "Your guess is smaller than the number!" << std::endl;
+        }
+
+        else {
+            std::cout << "Your guess is bigger than the number!" << std::endl;
+        }
+    }
+
+    return 0;
+}
