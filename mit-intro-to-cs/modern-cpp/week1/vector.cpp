62b9a1993b6ee3ee9be6d18c2f729e330c7ad453
brutist <jmmauring@up.edu.ph>
2024-08-16 11:56:37 +0800

    Initial commit for algorithmic toolbox course
    Change algorithms to algorithms and data structures, this folder
    will contain all courses related to algo and data structures

--- mit-intro-to-cs/modern-cpp/week1/vector.cpp
+++ mit-intro-to-cs/modern-cpp/week1/vector.cpp
@@ -4,5 +4,6 @@
 int main(int argc, char const *argv[]) {
     std::vector<int> vec = {1, 3, 2, 4};
     std::cout << vec.front() << " " << vec.back() << std::endl;
+    const std::string myString = "";
     return 0;
 }
