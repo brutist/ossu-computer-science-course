8249356e1ae02a6f60a3c3a2549f0b38d8a0eb09
brutist <jmmauring@up.edu.ph>
2024-08-16 16:39:22 +0800

    Implement a linear algorithm for max_pairwise

---
+++ algorithms-and-data-structures/ucsd-data-struc-and-algo-specialization/algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/data_producer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    int n = 200000;
+    srand(time(NULL));
+
+    // create and open a text file
+    std::ofstream fn("dataset.txt");
+
+    fn << n << "\n";
+
+    // writer to the file
+    for (int i = 0; i < n; i++) {
+        fn << std::rand() << " ";
+    }
+
+    fn.close();
+
+    return 0;
+}
--- algorithms-and-data-structures/ucsd-data-struc-and-algo-specialization/algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ algorithms-and-data-structures/ucsd-data-struc-and-algo-specialization/algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -2,28 +2,58 @@
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+int64_t MaxPairwiseProduct(const std::vector<int64_t >& numbers) {
+    int64_t max_product = 0;
     int n = numbers.size();
 
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                max_product = std::max(max_product, numbers[i] * numbers[j]);
+            }
         }
     }
 
     return max_product;
 }
 
-int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> numbers[i];
+int64_t MaxPairwiseProductFast(const std::vector<int64_t>& numbers) {
+    int max = 0;
+    int s_max = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        if (numbers[max] < numbers[i]) {
+            int temp = max;
+            max = i;
+            s_max = temp;
+        }
+        else if (max == s_max || numbers[s_max] < numbers[i]) {
+            s_max = i; 
+        }
     }
+  
+    return numbers[max] * numbers[s_max];
+}
 
-    std::cout << MaxPairwiseProduct(numbers) << "\n";
+const std::vector<int64_t> randomNumbers(int n) {
+    srand(time(NULL));
+    std::vector<int64_t> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        numbers[i] = rand() % 2000000;
+    }
+    return numbers;
+}
+
+int main() {
+    while (true) {
+        const std::vector<int64_t> numbers = randomNumbers(200000);
+        int64_t answer = MaxPairwiseProduct(numbers);
+        int64_t result = MaxPairwiseProductFast(numbers);
+        if (answer != result) {
+            std::cout << "Answer: " << answer << "   Result: " << result << "\n";
+            for (int64_t i : numbers)
+                std::cout << i << " ";
+            return 1;
+        }
+    }
     return 0;
 }
