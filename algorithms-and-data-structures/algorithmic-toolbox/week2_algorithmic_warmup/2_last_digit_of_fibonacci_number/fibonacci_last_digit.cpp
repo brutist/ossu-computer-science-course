b0fd7d2ae12b2ae76fa2c95780a54e4cf4a3a16c
brutist <jonathanmauring1998@gmail.com>
2024-08-17 16:10:46 +0800

    Add a fast implementation of a fibonacci nos. last digit

--- algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -16,9 +16,51 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+
+int get_fibonacci_last_digit_fast(int n) {
+    if (n <= 1)     
+        return n;
+
+    int previous = 0;
+    int current = 1;
+
+    for (int i = 0; i < n - 1; i++) {
+        int temp_previous = previous;
+        previous = current;
+        current = (temp_previous + current) % 10;
+    }
+
+    return current;
+}
+
+
+void stress_test_fibonacci_last_digit(int n) {
+    for (int i = 0; i < n + 1; i++) {
+        int naive_answer = get_fibonacci_last_digit_naive(i);
+        int fast_answer = get_fibonacci_last_digit_fast(i);
+
+        if (naive_answer != fast_answer) {
+            std::cout << "Answer: " << naive_answer << "  Result: " << fast_answer;
+            break;
+        }
+    }
+
+}
+
+void test_fast_fibonacci_last_digit(int n) {
+    for (int i = 0; i < n + 1; i++) {
+        int answer = get_fibonacci_last_digit_fast(i);
+
+        std::cout << "Fibonacci " << i << " : " << answer << "\n";
+    }
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
+
+    //stress_test_fibonacci_last_digit(n);
+    //test_fast_fibonacci_last_digit(n);
     }
