429f862aa060d4dc3f82f6ef08e8e7441e0d413b
brutist <jonathanmauring1998@gmail.com>
2024-08-17 23:24:22 +0800

    Add stress_test for fast implementation of lcm

--- algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/lcm.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/lcm.cpp
@@ -8,9 +8,46 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+// an implementation of Euclid GCD algorithm
+int gcd_fast(int a, int b) {
+  if (b == 0) return a;
+
+  int remainder_a = a % b;
+  return gcd_fast(b, remainder_a);
+}
+
+// the lcm is just the product divided by their greatest common divisor
+long long lcm_fast(int a, int b) {
+  return ((long long) a * b) / (long long) gcd_fast(a, b); 
+}
+
+void stress_test_lcm() {
+  srand(time(NULL));
+  unsigned test_counter = 0;
+  while (true) {
+    int a = std::rand() % 10000;
+    int b = std::rand() % 10000;
+  
+    long long naive_answer = lcm_naive(a, b);
+    long long fast_answer = lcm_fast(a, b);
+    
+    if (naive_answer != fast_answer) {
+      std::cout << "a: " << a << " b: " << b << " naive: " 
+                << naive_answer << "  fast: " << fast_answer;
+    }
+
+    test_counter++;
+    if (test_counter % 100 == 0) {
+      std::cout << "TEST " << test_counter << "  PASSED\n";
+    } 
+  }
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
+
+  stress_test_lcm();
   return 0;
 }
