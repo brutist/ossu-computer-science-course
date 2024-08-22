b9d38f139bf16857722f3a00b42a7d7d8cefa509
brutist <jonathanmauring1998@gmail.com>
2024-08-22 22:29:31 +0800

    Completed week 2 of algorithmic toolbox

--- algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/fibonacci_last_digit.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/fibonacci_last_digit.cpp
@@ -11,46 +11,61 @@ int get_fibonacci_last_digit_naive(int n) {
     for (int i = 0; i < n - 1; ++i) {
         int tmp_previous = previous;
         previous = current;
-        current = tmp_previous + current;
-    }
-
-    return current % 10;
-}
-
-
-int get_fibonacci_last_digit_fast(int n) {
-    if (n <= 1)     
-        return n;
-
-    int previous = 0;
-    int current = 1;
-
-    for (int i = 0; i < n - 1; i++) {
-        int temp_previous = previous;
-        previous = current;
-        current = (temp_previous + current) % 10;
+        current = (tmp_previous + current) % 10;
     }
 
     return current;
 }
 
 
-void stress_test_fibonacci_last_digit(int n) {
-    if (n > 50) {
-        throw std::invalid_argument("stress_test_fibonacci_last_digit cannot take args > 200");
+int get_fibonacci_last_digit_fast(int n) {
+    if (n <= 1)
+        return n;
+
+    // We know that the pisano length of modulo 10 is 60
+    int M = 10;
+    int pisano_ten_length = 60;
+
+    // to calculate the last digit of F(n) we can look at the its corresponding 
+    // position in the pisano sequence (M = 10).
+    int equivalence = n % pisano_ten_length;
+    if (equivalence == 0)
+        equivalence = pisano_ten_length;
+
+    int previous = 0;
+    int current = 1;
+    for (int i = 0; i < equivalence - 1; i++) {
+        int temp_previous = previous;
+        previous = current;
+        current = (temp_previous + previous) % M;
     }
+
+    return current;
+}
+
+
+void stress_test_fibonacci_last_digit() {
+    srand(time(NULL));
+    unsigned int test_counter = 0;
     
-    for (int i = 0; i < n + 1; i++) {
-        int naive_answer = get_fibonacci_last_digit_naive(i);
-        int fast_answer = get_fibonacci_last_digit_fast(i);
+    while (true) {
+        int n = rand() % 40;
+
+        int naive_answer = get_fibonacci_last_digit_naive(n);
+        int fast_answer = get_fibonacci_last_digit_fast(n);
 
         if (naive_answer != fast_answer) {
-            std::cout << "Fibonacci of :" << i << "Answer: " << naive_answer 
+            std::cout << "Fibonacci of: " << n << "  Answer: " << naive_answer 
                         << "  Result: " << fast_answer;
             break;
         }
-    }
 
+        test_counter++;
+        if (test_counter % 1000 == 0)
+        {
+            std::cout << "TEST " << test_counter << "  PASSED\n";
+        }
+    }
 }
 
 void test_fast_fibonacci_last_digit(int n) {
@@ -67,6 +82,6 @@ int main() {
     int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
 
-    //stress_test_fibonacci_last_digit(n);
+    stress_test_fibonacci_last_digit();
     //test_fast_fibonacci_last_digit(n);
     }
--- algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/fibonacci_sum_squares.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week2_algorithmic_warmup/fibonacci_sum_squares.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+int fibonacci_sum_squares_naive(long long n);
+int fibonacci_last_digit(long long n);
+int fibonacci_sum_squares_fast(long long n);
+void stress_test_fibonacci_sum_squares();
+
+int main() {
+    long long n = 0;
+    std::cin >> n;
+    std::cout << fibonacci_sum_squares_naive(n);
+
+    stress_test_fibonacci_sum_squares();
+}
+
+
 int fibonacci_sum_squares_naive(long long n) {
     if (n <= 1)
         return n;
@@ -11,15 +25,65 @@ int fibonacci_sum_squares_naive(long long n) {
     for (long long i = 0; i < n - 1; ++i) {
         long long tmp_previous = previous;
         previous = current;
-        current = tmp_previous + current;
-        sum += current * current;
+        current = (tmp_previous + current) % 10;
+        sum = (sum + (current * current)) % 10 ;
     }
 
-    return sum % 10;
+    return sum;
 }
 
-int main() {
-    long long n = 0;
-    std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+// Approach: Apparently, ∑F(n)^2  = F(n) x F(n + 1)
+int fibonacci_sum_squares_fast(long long n) {
+    int fib_n_last = fibonacci_last_digit(n);
+    int fib_n1_last = fibonacci_last_digit(n + 1);
+    return (fib_n_last * fib_n1_last) % 10;
 }
+
+
+int fibonacci_last_digit(long long n) {
+    if (n <= 1)
+        return n;
+
+    // We know that the pisano length of modulo 10 is 60
+    int M = 10;
+    int pisano_ten_length = 60;
+
+    // to calculate the last digit of F(n) we can look at the its corresponding 
+    // position in the pisano sequence (M = 10).
+    int equivalence = n % pisano_ten_length;
+    if (equivalence == 0)
+        equivalence = pisano_ten_length;
+
+    int previous = 0;
+    int current = 1;
+    for (int i = 0; i < equivalence - 1; i++) {
+        int temp_previous = previous;
+        previous = current;
+        current = (temp_previous + previous) % M;
+    }
+
+    return current;
+}
+
+void stress_test_fibonacci_sum_squares() {
+    srand(time(NULL));
+    unsigned int test_counter = 0;
+    while (true) {
+        const long long n = (rand() % 100000) + 1;
+        long long naive_answer = fibonacci_sum_squares_naive(n);
+        long long fast_answer = fibonacci_sum_squares_fast(n);
+
+        if (naive_answer != fast_answer){
+            std::cout << "Fibonacci sum of squares n: " << n << "  answer: " 
+                        << naive_answer << "  result: " << fast_answer << "\n";
+            break;
+        }
+
+        test_counter++;
+        if (test_counter % 1000 == 0)
+        {
+            std::cout << "TEST " << test_counter << "  PASSED\n";
+        }
+    }
+}
+
