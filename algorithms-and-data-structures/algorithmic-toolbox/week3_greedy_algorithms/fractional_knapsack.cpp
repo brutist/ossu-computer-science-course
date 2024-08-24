933b70158282677d31e37901efcdd5b7a88cfc3a
brutist <jonathanmauring1998@gmail.com>
2024-08-24 21:54:09 +0800

    Implement a greedy solution for the fractional knapsack problem

--- algorithms-and-data-structures/algorithmic-toolbox/week3_greedy_algorithms/fractional_knapsack.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week3_greedy_algorithms/fractional_knapsack.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 #include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int compare_value_per_unit(int value1, int weight1, int value2, int weight2);
+int compare_value_per_unit(double val_per_unit_1, double val_per_unit_2);
 bool equal_values(double value1, double value2, double precision);
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values);
 int get_most_weighted_value(vector<double> weights, vector<double> values);
@@ -23,14 +24,12 @@ int main() {
 
     double optimal_value = get_optimal_value(capacity, weights, values);
 
-    std::cout.precision(10);
-    std::cout << optimal_value << std::endl;
+    std::cout << fixed << setprecision(4) << optimal_value << std::endl;
     return 0;
 }
 
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
-    double value = 0.0;
     // create a deep copy of the weights and values vectors
     vector<double> weights_copy;
     vector<double> values_copy;
@@ -42,20 +41,28 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
         values_copy.push_back(values[i]);
     }
 
+    double value = 0.0;
+    double total_weight = 0.0;
     // Approach: Greedily fit the item that has the biggest value per unit.
     //          Repeat the process until knapsack is full or there are no items left.
-    while (value < capacity) {
-        double remaining_capacity = capacity - value;
+    while (total_weight < capacity) {
+        double remaining_capacity = capacity - total_weight;
 
         // find the index of an item that has the biggest value per unit
         int max = get_most_weighted_value(weights_copy, values_copy);
 
+        // if max is -1 then, there are no remaining items to take
+        if (max < 0)    break;
+
         double current_max_weight = weights_copy[max];
         double weight_to_add = min(current_max_weight, remaining_capacity);
-        value += (weight_to_add  * (values_copy[max] / weights_copy[max]));
+        double value_per_unit_max = values_copy[max] / weights_copy[max];
+        value += (weight_to_add * value_per_unit_max);
 
+        // add the item to the knapsack
+        total_weight = total_weight + weight_to_add;
         // subtract the weight added to knapsack to the weight of available things
-        weights_copy[max] -= weight_to_add;
+        weights_copy[max] = weights_copy[max] - weight_to_add;
     }
 
     return value;
@@ -63,11 +70,20 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
 
 int get_most_weighted_value(vector<double> weights, vector<double> values) {
     int N = weights.size();
-    int min_index = 0;      
+    int min_index = -1;      
     for (int i = 0; i < N; i++) {
-        int m = min_index;
-        if (compare_value_per_unit(values[m], weights[m], values[i], weights[i]) < 0) {
+        if (weights[i] <= 0) continue;
+
+        else if (min_index < 0) {
             min_index = i;
+        }
+
+        else {
+            double vpu1 = values[min_index] / weights[min_index];
+            double vpu2 = values[i] / weights[i];
+            if (compare_value_per_unit(vpu1, vpu2) < 0) {
+                min_index = i;
+            }
         }
     }
 
@@ -75,13 +91,7 @@ int get_most_weighted_value(vector<double> weights, vector<double> values) {
 }
 
 
-int compare_value_per_unit(int value1, int weight1, int value2, int weight2) {
-    // items that have zero weights are not considered
-    if (weight2 <= 0)   return 1;
-
-    double val_per_unit_1 = value1 / weight1;
-    double val_per_unit_2 = value2 / weight2;
-
+int compare_value_per_unit(double val_per_unit_1, double val_per_unit_2) {
     return val_per_unit_1 - val_per_unit_2;
 }
 
