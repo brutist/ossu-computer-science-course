84bfa868576d7222462e086577a84019c78a32a7
brutist <jmmauring@up.edu.ph>
2024-08-27 10:46:00 +0800

    Add lecture code

---
+++ algorithms-and-data-structures/algorithmic-toolbox/week4_divide_and_conquer/polynomial_multiplication.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> poly_mult(vector<int> A, vector<int> B, int n) {
+    int product_size = (2 * n) - 1;
+    vector<int> product(product_size, 0);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            product[i + j] += (A[i] * B[j]);
+        }
+    }
+
+    return product;
+}
+
+int main() {
+    int n;
+    vector<int> A(n);
+    vector<int> B(n);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i] >> B[i];
+    }
+
+    for (int i : poly_mult(A, B, n)) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
