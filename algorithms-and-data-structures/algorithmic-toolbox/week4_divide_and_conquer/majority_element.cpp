4cf08fec88dd440610858ddc2f79bf813481be44
brutist <jonathanmauring1998@gmail.com>
2024-08-31 00:06:53 +0800

    Solve majority element in nlogn

--- algorithms-and-data-structures/algorithmic-toolbox/week4_divide_and_conquer/majority_element.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week4_divide_and_conquer/majority_element.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
 // return the majority element in the vector (strictly > (n/2))
 int get_majority_element_naive(vector<int> &a) {
@@ -25,6 +25,80 @@ int get_majority_element_naive(vector<int> &a) {
     return -1;
 }
 
+int get_majority_element(vector<int> &a) {
+    // Approach: Sort the vector first, and do a linear pass to identify if 
+    //          an element is a majority element.
+    vector<int> a_copy = a;
+    sort(a_copy.begin(), a_copy.end());
+
+    int n = a.size();
+    int majority_threshold = n / 2;
+    int element_checking;
+    int element_checking_count;
+    for (int i = 0; i < n; i++) {
+        if (i == 0) {
+            element_checking = a_copy[i];
+            element_checking_count = 1;
+        }
+
+        else {
+            // check if the current element is still the same with what's 
+            //  being checked
+            if (element_checking == a_copy[i]) {
+                element_checking_count++;
+            }
+            else {
+                element_checking = a_copy[i];
+                element_checking_count = 1;
+            }
+        }
+
+        if (element_checking_count > majority_threshold) {
+            return element_checking;
+        }
+    }
+
+    return -1;
+}
+
+void stress_test_majority_element() {
+    srand(time(NULL));
+    unsigned int test_counter = 0;
+    int N_LIMIT = 1000;
+    int VALUE_LIMIT = 1000;
+
+    while (true) {
+        const int n = (rand() % N_LIMIT) + 1;
+        vector<int> search_array(n);
+
+        for (int i = 0; i < n; i++) {
+            search_array[i] = (rand() % VALUE_LIMIT) + 1;
+        }
+
+        int naive_answer = get_majority_element_naive(search_array);
+        int fast_answer = get_majority_element(search_array);
+        if (naive_answer != fast_answer) {
+            for (int num : search_array) {
+                cout << num << " ";
+            }
+            cout << "\n";
+            cout << "Majority Element of n: " << n
+                << "  answer: " << naive_answer
+                << "  result: " << fast_answer << "\n";
+
+            break;
+        }
+
+        test_counter++;
+        if (test_counter % 10000 == 0) {
+            cout << "TEST " << test_counter << "  PASSED\n";
+        }
+    
+    }
+}
+
+
+
 int main() {
     int n;
     std::cin >> n;
@@ -33,4 +107,6 @@ int main() {
         std::cin >> a[i];
     }
     std::cout << (get_majority_element_naive(a) != -1) << '\n';
+
+    stress_test_majority_element();
 }
