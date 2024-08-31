0a04327a7a35a2291704cdbd8af8feaf31958c63
brutist <jonathanmauring1998@gmail.com>
2024-08-31 11:35:47 +0800

    Formatting changes via clang-formatter.sh

--- algorithms-and-data-structures/algorithmic-toolbox/week4_divide_and_conquer/majority_element.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week4_divide_and_conquer/majority_element.cpp
@@ -26,7 +26,7 @@ int get_majority_element_naive(vector<int> &a) {
 }
 
 int get_majority_element_fast(vector<int> &a) {
-    // Approach: Sort the vector first, and do a linear pass to identify if 
+    // Approach: Sort the vector first, and do a linear pass to identify if
     //          an element is a majority element.
     vector<int> a_copy = a;
     sort(a_copy.begin(), a_copy.end());
@@ -42,12 +42,11 @@ int get_majority_element_fast(vector<int> &a) {
         }
 
         else {
-            // check if the current element is still the same with what's 
+            // check if the current element is still the same with what's
             //  being checked
             if (element_checking == a_copy[i]) {
                 element_checking_count++;
-            }
-            else {
+            } else {
                 element_checking = a_copy[i];
                 element_checking_count = 1;
             }
@@ -62,9 +61,11 @@ int get_majority_element_fast(vector<int> &a) {
 }
 
 int get_majority_element_improved_helper(vector<int> &a, int start, int end) {
-    // Approach: Recusively search for the majority element of each half of the vector.
-    //          Divide the array into two halves. An element is the majority of 
-    //          the whole array if it is at least the majority of one of the halves.
+    // Approach: Recusively search for the majority element of each half of the
+    // vector.
+    //          Divide the array into two halves. An element is the majority of
+    //          the whole array if it is at least the majority of one of the
+    //          halves.
 
     // the element in a single-item vector is the majority element
     if (start == end) {
@@ -79,19 +80,23 @@ int get_majority_element_improved_helper(vector<int> &a, int start, int end) {
     int left_majority_count = 0;
     int right_majority_count = 0;
     for (int i = start; i <= end; i++) {
-        if (left_majority == a[i])          left_majority_count++;
-        else if (right_majority == a[i])    right_majority_count++;
+        if (left_majority == a[i])
+            left_majority_count++;
+        else if (right_majority == a[i])
+            right_majority_count++;
     }
 
     int half_of_total_elements = ((end - start) + 1) / 2;
     if (left_majority_count > half_of_total_elements) {
         return left_majority;
     }
+
     else if (right_majority_count > half_of_total_elements) {
         return right_majority;
     }
+
     else {
-        return -1;  // no majority element
+        return -1; // no majority element
     }
 }
 
@@ -123,8 +128,8 @@ void stress_test_majority_element() {
             }
             cout << "\n";
             cout << "Majority Element of n: " << n
-                 << "  answer: " << naive_answer
-                 << "  fast: "   << fast_answer << "\n";
+                 << "  answer: " << naive_answer << "  fast: " << fast_answer
+                 << "\n";
 
             break;
         }
@@ -161,7 +166,8 @@ vector<double> time_get_majority_element() {
 
     double naive_start_time = (double)clock() / CLOCKS_PER_SEC;
     get_majority_element_fast(search_array);
-    double naive_time_diff = ((double)clock() / CLOCKS_PER_SEC) - naive_start_time;
+    double naive_time_diff =
+        ((double)clock() / CLOCKS_PER_SEC) - naive_start_time;
 
     double start_time = (double)clock() / CLOCKS_PER_SEC;
     get_majority_element(search_array);
@@ -186,7 +192,7 @@ void average_time_get_majority_element(int samples) {
 
     std::cout << "The average time elapsed for fast with max input: "
               << fast_time / samples << "\n";
-    std::cout << "The average time elapsed for faster with max input: " 
+    std::cout << "The average time elapsed for faster with max input: "
               << faster_time / samples << "\n";
 }
 
@@ -200,5 +206,5 @@ int main() {
     std::cout << (get_majority_element_naive(a) != -1) << '\n';
 
     stress_test_majority_element();
-    //average_time_get_majority_element(1000);
+    // average_time_get_majority_element(1000);
 }
