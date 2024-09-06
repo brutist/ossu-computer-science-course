3b2356b73728b48b5762e41d6b721f70011f28dc
not-a-legend <jonathanmauring1998@gmail.com>
2024-09-06 12:17:47 +0800

    Save work on knapsack problem

--- algorithms-and-data-structures/algorithmic-toolbox/week6_dynamic_programming2/knapsack.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week6_dynamic_programming2/knapsack.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 
 using std::vector;
+using std::max;
 
-int optimal_weight(int W, const vector<int> &w) {
-    // write your code here
+int optimal_weight_greedy(int W, const vector<int> &w) {
     int current_weight = 0;
     for (size_t i = 0; i < w.size(); ++i) {
         if (current_weight + w[i] <= W) {
@@ -14,6 +14,17 @@ int optimal_weight(int W, const vector<int> &w) {
     return current_weight;
 }
 
+int optimal_weight__naive_util(int W, int taken ,vector<int> w, int i) {
+    if (taken + w[i] > W || i < 0)     return W;
+
+    return max(optimal_weight__naive_util(W, taken + w[i], w, i - 1), 
+               optimal_weight__naive_util(W, taken, w, i - 1));
+}
+
+int optimal_weight_naive(int W, const vector<int> &w) {
+    return optimal_weight__naive_util(W, 0,w, w.size() - 1);
+}
+
 int main() {
     int n, W;
     std::cin >> W >> n;
@@ -21,5 +32,5 @@ int main() {
     for (int i = 0; i < n; i++) {
         std::cin >> w[i];
     }
-    std::cout << optimal_weight(W, w) << '\n';
+    std::cout << optimal_weight_naive(W, w) << '\n';
 }
