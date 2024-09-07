4ccf54419c5da99c2c2745ce66a90fe1ba0818da
brutist <jonathanmauring1998@gmail.com>
2024-09-07 22:40:35 +0800

    Save work on partition3

--- algorithms-and-data-structures/algorithmic-toolbox/week6_dynamic_programming2/partition3.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week6_dynamic_programming2/partition3.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 
 using std::vector;
+using std::accumulate;
 
-int partition3(vector<int> &A) {
-    // write your code here
-    return 0;
+int partition3_naive_util(vector<int> &A, int total) {
+    // Approach: If all of the items in a have been considered,
+    //          either in setA, setB, or setC. The total for 
+}
+
+int partition3_naive(vector<int> &A) {
+    auto sum = accumulate(A.begin(), A.end());
+    
+    if (sum % 3 != 0) {
+        int no_equal_partition = -1;
+        return no_equal_partition;
+    }
+
+    return partition3_naive_util();
 }
 
 int main() {
