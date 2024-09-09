b6614a22f1701a5831cf2aba785b9bb05515409b
brutist <jonathanmauring1998@gmail.com>
2024-09-09 23:21:31 +0800

    Solve partition3 with some hint

--- algorithms-and-data-structures/algorithmic-toolbox/week6_dynamic_programming2/partition3.cpp
+++ algorithms-and-data-structures/algorithmic-toolbox/week6_dynamic_programming2/partition3.cpp
@@ -13,69 +13,29 @@ class SumPartition {
   public:
     vector<int> &A;
 
-    int optimal_weight(int W, vector<int> &included) {
-        // returns the optimal weight that can be fitted
-        //  into the knapsack with capacity W. This function
-        //  also mutates the included vector to update the
-        //  index of the items that has been included to the
-        //  optimal weight.
-        int num_items = A.size();
-        vector<vector<int>> dp(W + 1, vector<int>(num_items + 1));
-        for (int w = 0; w <= W; w++) {
-            for (int i = 0; i <= num_items; i++) {
-                if (w == 0 || i == 0) {
-                    dp[w][i] = 0;
-                }
+    int partition3() {
+        int sum = accumulate(A.begin(), A.end(), 0);
+        if (sum % 3 != 0) {
+            return false;
+        }
+        int size = A.size();
 
-                dp[w][i] = dp[w][i - 1];
-                if (A[i - 1] <= w && included[i - 1] == 0) {
-                    int val = dp[w - A[i - 1]][i - 1] + A[i - 1];
+        vector<vector<int>> dp(sum + 1, vector<int>(sum + 1, 0));
+        dp[0][0] = true;
 
-                    if (dp[w][i] < val) {
-                        dp[w][i] = val;
+        // process the numbers one by one
+        for (int i = 0; i < size; i++) {
+            for (int j = sum; j >= 0; --j) {
+                for (int k = sum; k >= 0; --k) {
+                    if (dp[j][k]) {
+                        dp[j + A[i]][k] = true;
+                        dp[j][k + A[i]] = true;
                     }
                 }
             }
         }
-        // backtrack to identify the included items and update
-        //  included vector
-        for (int n = num_items, w = W; n > 0; n--) {
-            if (dp[w][n] > dp[w][n - 1]) {
-                w = dp[w][n] - A[n - 1];
-                included[n - 1] = 1;
-            }
-        }
-
-        return dp[W][num_items];
-    }
-
-    int partition3() {
-        // Approach: The set of int can only be partitioned to three
-        //          equal parts if the sum of the set is divisible
-        //          by three. If it is possible to partition the set
-        //          into three equal parts, we can identify if it
-        //          is possible to fill in three 0/1 knapsack of
-        //          weight = sum / 3.
-        int sum = accumulate(A.begin(), A.end(), 0);
-        if (sum % 3 != 0) {
-            return 0;
-        }
-
-        // keep track of the included integers in the three sets
-        int partition = sum / 3;
-        vector<int> included(A.size() + 1, 0); // 1-based index
-        // if the weights of all the sets is equal to the sum / 3
-        // then there is a way to partition such that the partitions
-        // have equal sums
-        int setA = optimal_weight(partition, included);
-        int setB = optimal_weight(partition, included);
-        int setC = optimal_weight(partition, included);
-
-        if (setA == setB && setB == setC && setC == partition) {
-            return 1;
-        }
-
-        return 0;
+        
+        return dp[sum / 3][sum / 3];
     }
 
     bool canPartition() {
@@ -124,7 +84,7 @@ void print_array(vector<int> v, string name) {
 void stress_test_partition3(bool verbose = true) {
     srand(time(NULL));
     unsigned int test_counter = 0;
-    int N_LIMIT = 10;
+    int N_LIMIT = 20;
     int MIN_VALUE = 1;
     int MAX_VALUE = 30;
     int FAILED_TEST = 0;
@@ -162,6 +122,21 @@ void stress_test_partition3(bool verbose = true) {
     }
 }
 
+void time_partition3() {
+    int n = 20;
+    int MIN_VALUE = 1;
+    int MAX_VALUE = 30;
+    vector<int> weights = generate_random_vector(n, MIN_VALUE, MAX_VALUE);
+    SumPartition sp = {weights};
+
+    double start_time = (double)clock() / CLOCKS_PER_SEC;
+    sp.partition3();
+    double time_diff = ((double)clock() / CLOCKS_PER_SEC) - start_time;
+
+    std::cout << "The time elapsed for fast partition3 with max input: "
+              << time_diff << "\n";
+}
+
 int main() {
     int n;
     std::cin >> n;
@@ -174,4 +149,5 @@ int main() {
     std::cout << sp.partition3() << '\n';
 
     stress_test_partition3();
+    //time_partition3();
 }
