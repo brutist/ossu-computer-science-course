325f475d5073fe851188e4b0dc90b1bd4c2bdc4b
brutist <jmmauring@up.edu.ph>
2024-09-13 16:09:10 +0800

    Solve tree-height.cpp

--- algorithms-and-data-structures/data-structures-course-2/week1_basic_data_structures/2_tree_height/tree-height.cpp
+++ algorithms-and-data-structures/data-structures-course-2/week1_basic_data_structures/2_tree_height/tree-height.cpp
@@ -21,31 +21,45 @@ class Node {
     }
 };
 
+int max_tree_height(Node *root, int height) {
+    if (root->children.size() == 0) {
+        return height;
+    }
+    
+    int max_height = 0;
+    for (Node *r : root->children) {
+        int child_height = max_tree_height(r, height + 1);
+        if (max_height < child_height) {
+            max_height = child_height;
+        }
+    }
+
+    return max_height;
+}
+
 int main_with_large_stack_space() {
     std::ios_base::sync_with_stdio(0);
     int n;
     std::cin >> n;
 
     std::vector<Node> nodes;
+    int root_index;
     nodes.resize(n);
     for (int child_index = 0; child_index < n; child_index++) {
         int parent_index;
         std::cin >> parent_index;
+
         if (parent_index >= 0)
             nodes[child_index].setParent(&nodes[parent_index]);
+
+        // identify the index of the root node
+        else
+            root_index = child_index;
+
         nodes[child_index].key = child_index;
     }
 
-    // Replace this code with a faster implementation
-    int maxHeight = 0;
-    for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-        int height = 0;
-        for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-            height++;
-        maxHeight = std::max(maxHeight, height);
-    }
-
-    std::cout << maxHeight << std::endl;
+    std::cout << max_tree_height(&nodes[root_index], 1) << std::endl;
     return 0;
 }
 
