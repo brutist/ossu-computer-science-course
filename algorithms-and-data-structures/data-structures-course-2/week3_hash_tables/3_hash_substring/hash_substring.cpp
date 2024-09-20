19e88c62b67021db6d67950db72fd06081c284e9
brutist <jmmauring@up.edu.ph>
2024-09-20 12:52:23 +0800

    Save work on hash_substring

--- algorithms-and-data-structures/data-structures-course-2/week3_hash_tables/3_hash_substring/hash_substring.cpp
+++ algorithms-and-data-structures/data-structures-course-2/week3_hash_tables/3_hash_substring/hash_substring.cpp
@@ -1,7 +1,7 @@
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cmath>
 
 using std::string;
 typedef unsigned long long ull;
@@ -16,8 +16,10 @@ Data read_input() {
     return data;
 }
 
-ull hash_func(const string &s, ull multiplier, ull prime) {
-    ull hash = 0;
+size_t hash_func(const string &s) {
+    static const size_t multiplier = 263;
+    static const size_t prime = 1000000007;
+    unsigned long long hash = 0;
     for (int i = static_cast<int>(s.size()) - 1; i >= 0; --i)
         hash = (hash * multiplier + s[i]) % prime;
     return hash;
@@ -29,44 +31,48 @@ void print_occurrences(const std::vector<int> &output) {
     std::cout << "\n";
 }
 
-std::vector<int> get_occurrences(const Data &input) {
+ull base_raise_to(int B, size_t P, size_t prime) {
+    ull h = 1;
+    for (size_t i = 0; i < P; i++) {
+        h = (h * B) % prime;
+    }
+    return h;
+}
+
+std::vector<size_t> precompute_hashes(const Data &input, size_t p, size_t b) {
     // Approach: Calculate the hash value of the pattern. Calculate
     //      the hash value of the fist substring of the text that is
     //      the same length as pattern. Slide over the text and
     //      calculate the hash value of hash(i+1) from hash(i) by
     //      subracting the contribution of text[i] and adding the
     //      contribution of text[i + 1 + pattern_length] to the hash.
+    int T = input.text.size();
+    int P = input.pattern.size();
+    std::vector<size_t> H(T - P + 1);
+    H[T - P] = hash_func(input.text.substr(T - P, P));
+    ull y = base_raise_to(b, P, p);
+    
+    for (int i = T - P - 1; i >= 0; i--) {
+        H[i] = ((b * H[i + 1]) + input.text[i] - (y * input.text[i + P])) % p;
+    }
 
-    // choose suitable base and modulo
-    ull B = 263;
-    ull P = 1000000007;
+    return H;
+}
 
-    // calculate the hash value of the pattern
-    ull pattern_hash = hash_func(input.pattern, B, P);
-
-    // calculate the hash value of the first substring of the text
-    //  that is the same length as pattern
-    size_t pattern_length = input.pattern.size();
+std::vector<int> get_occurrences(const Data &input) {
+    // choose suitable (B)ase and (P)rime
+    size_t B = 263;
+    size_t P = 1000000007;
     std::vector<int> occurrences;
-    ull hash;
-    for (size_t i = 0; i + pattern_length < input.text.size(); i++) {
-        // calculate the first substring hash
-        if (i == 0) {
-            string first_substring = input.text.substr(i, pattern_length);
-            hash = hash_func(first_substring, B, P);
-        }
-
-        // update hash[i+1] by subtracting contribution of text[i] and adding
-        //  contribution of text[i + 1 +  pattern_length]
-        else {
-            hash = (hash - (input.text[i - pattern_length] * (std::pow(B, pattern_length - 1))) % P) * B + input.text[i];
-        }
-
+    ull pattern_hash = hash_func(input.pattern);
+    // precalculate the hashes for substring from 0 to txt[i - len(pattern)]
+    std::vector<size_t> hashes = precompute_hashes(input, P, B);
+    for (size_t i = 0; i < hashes.size(); i++) {
         // check if substring is really the same as the pattern or just a
         //  hash collision
-        if (hash == pattern_hash &&
-            input.text.substr(i, pattern_length) == input.pattern) {
-            occurrences.push_back(i);
+        if (hashes[i] == pattern_hash) {
+            if (input.text.substr(i, input.pattern.size()) == input.pattern)
+                occurrences.push_back(i);
         }
     }
 
