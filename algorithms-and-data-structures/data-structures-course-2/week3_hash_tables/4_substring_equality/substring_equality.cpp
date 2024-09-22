ea821d5044779357b83602a4ef5c5b39038352a5
brutist <jonathanmauring1998@gmail.com>
2024-09-22 14:04:04 +0800

    Save work on substring_equality

--- algorithms-and-data-structures/data-structures-course-2/week3_hash_tables/4_substring_equality/substring_equality.cpp
+++ algorithms-and-data-structures/data-structures-course-2/week3_hash_tables/4_substring_equality/substring_equality.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solver {
     string s;
+    std::vector<int> H1;
+    std::vector<int> H2;
+    int x;
 
   public:
-    Solver(string s) : s(s) {
-        // initialization, precalculation
+    Solver(string s) : s(s), H1(s.size(), 0), H2(s.size(), 0), x(0) {
+        // Approach: Suppose a polynomial hash function is defined as H(s,e),
+        // where s is the starting index and e is the end index of a substring.
+        // The hash of a substring H(a, a+l) is equal to the H(0, l) - H(0, a).
+        // That is the hash of a substring is the difference between the hash
+        // from 0 to l minus the hash from 0 - a. We can use this property of
+        // poly hash to precalculate the H(0,n) for each substring of length n.
+
+        // pick the base and modulo
+        int m1 = 1000000007;
+        int m2 = 1000000009;
+        // select a random base
+        srand(time(NULL));
+        x = (rand() % 1000000000) + 1;
+
+        // compute the hashes of H(0, n) H1 using m1, and H2 using m2 as primes
+        int S = s.size();
+        for (int i = 1; i < S; i++) {
+            H1[i] = (s[i] + (x * H1[i - 1])) % m1;
+            H2[i] = (s[i] + (x * H2[i - 1])) % m2;
+        }
     }
-    bool ask(int a, int b, int l) { return s.substr(a, l) == s.substr(b, l); }
+
+    bool ask(int a, int b, int l) {
+        int hash_a1 = H1[a + l - 1] - H1[a];
+        int hash_a2 = H2[a + l - 1] - H2[a];
+        int hash_b1 = H1[b + l - 1] - H1[b];
+        int hash_b2 = H2[b + l - 1] - H2[b];
+        // if both hash of the substrings are the same, then they are most
+        //  probably equal strings
+        return hash_a1 == hash_b1 && hash_a2 == hash_b2;
+    }
 };
 
 int main() {
