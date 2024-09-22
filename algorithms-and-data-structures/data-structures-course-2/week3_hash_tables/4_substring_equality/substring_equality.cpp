7808da3740321d910f784064617c7665ca4531c7
brutist <jonathanmauring1998@gmail.com>
2024-09-22 14:38:41 +0800

    Save work on substring_equality

--- algorithms-and-data-structures/data-structures-course-2/week3_hash_tables/4_substring_equality/substring_equality.cpp
+++ algorithms-and-data-structures/data-structures-course-2/week3_hash_tables/4_substring_equality/substring_equality.cpp
@@ -27,17 +27,26 @@ class Solver {
 
         // compute the hashes of H(0, n) H1 using m1, and H2 using m2 as primes
         int S = s.size();
-        for (int i = 1; i < S; i++) {
-            H1[i] = (s[i] + (x * H1[i - 1])) % m1;
-            H2[i] = (s[i] + (x * H2[i - 1])) % m2;
+        for (int i = 1; i <= S; i++) {
+            // H1[i] = (s[i] + (x * H1[i - 1])) % m1;
+            // H2[i] = (s[i] + (x * H2[i - 1])) % m2;
+            H1[i] = hash_func(s.substr(0, i), m1, x);
+            H2[i] = hash_func(s.substr(0, i), m2, x);
         }
     }
 
+    int hash_func(const string &s, int p, int b) {
+        unsigned long long hash = 0;
+        for (int i = static_cast<int>(s.size()) - 1; i >= 0; --i)
+            hash = (hash * b + s[i]) % p;
+        return hash;
+    }
+
     bool ask(int a, int b, int l) {
-        int hash_a1 = H1[a + l - 1] - H1[a];
-        int hash_a2 = H2[a + l - 1] - H2[a];
-        int hash_b1 = H1[b + l - 1] - H1[b];
-        int hash_b2 = H2[b + l - 1] - H2[b];
+        int hash_a1 = H1[a + l] - H1[a];
+        int hash_a2 = H2[a + l] - H2[a];
+        int hash_b1 = H1[b + l] - H1[b];
+        int hash_b2 = H2[b + l] - H2[b];
         // if both hash of the substrings are the same, then they are most
         //  probably equal strings
         return hash_a1 == hash_b1 && hash_a2 == hash_b2;
